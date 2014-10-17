78f62a18d7fd1203725611ff5f4726eb1388224b
Timothée Lottaz <timozattol@gmail.com>
2014-10-17 17:18:05 +0200

    Add a new empty Test Project in tests/ directory

---
+++ ProjectRadinAndroidApp/tests/src/ch/epfl/sweng/radin/test/DummyTest.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package ch.epfl.sweng.radin.test;
+
+
+import android.test.AndroidTestCase;
+
+
+/**
+ * @author timozattol
+ * This is a "dummy" test, just to try out.
+ */
+public class DummyTest extends AndroidTestCase {
+    public void alwaysTrueTest() {
+        assert true;
+    }
+}
