b5871cba517455b95acea4f6d4647e59c67a3199
Fabien Zellweger <fabien.zellweger@epfl.ch>
2014-10-20 13:42:56 +0200

    create the ActionBar class

---
+++ ProjectRadinAndroidApp/src/ch/epfl/sweng/radin/ActionBar.java
@@ -0,0 +1,14 @@
+package ch.epfl.sweng.radin;
+
+import android.content.Context;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+public class ActionBar {
+	
+	public static void addActionBar(Context context, RelativeLayout relativeL){
+		Button[] actionBarContent = new Button[5];
+		
+	}
+
+}
