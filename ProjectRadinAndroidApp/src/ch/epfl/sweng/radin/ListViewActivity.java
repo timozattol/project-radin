c9823940f865668dabc7f42f9e87f75b4c43b1f5
Fabien Zellweger <Fabien.Zellweger@epfl.ch>
2014-10-20 21:06:13 +0200

    add link from login to register and from listView to notifications

--- ProjectRadinAndroidApp/src/ch/epfl/sweng/radin/ListViewActivity.java
+++ ProjectRadinAndroidApp/src/ch/epfl/sweng/radin/ListViewActivity.java
@@ -3,6 +3,9 @@ package ch.epfl.sweng.radin;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -23,6 +26,17 @@ public class ListViewActivity extends Activity {
 
 		RelativeLayout thisLayout = (RelativeLayout) findViewById(R.id.listViewListLayout);
 		ActionBar.addActionBar(this, thisLayout, mListTitle);
+		
+		Button notificationsBtn = (Button) findViewById(R.id.notificationButton);
+		notificationsBtn.setOnClickListener(notificationsButtonListener);
 	}
+	
+	private OnClickListener notificationsButtonListener = new View.OnClickListener() {		
+		@Override
+		public void onClick(View v) {
+			Intent displayActivityIntent = new Intent(v.getContext(), NotificationsActivity.class);
+	        startActivity(displayActivityIntent);			
+		}
+	};
 
 }
