3a960c04f93c0cd65274788caec1352e9de8c14e
Simon Le Bail-Collet <simon.lebail-collet@epfl.ch>
2014-10-20 21:46:07 +0200

    Sign up button swaps registerActivity to HomeActivity if User Data is good

--- ProjectRadinAndroidApp/src/ch/epfl/sweng/radin/RegisterActivity.java
+++ ProjectRadinAndroidApp/src/ch/epfl/sweng/radin/RegisterActivity.java
@@ -2,18 +2,21 @@ package ch.epfl.sweng.radin;
 
 import ch.epfl.sweng.radin.R.id;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * @author Simonchelbc
- *
+ * 
  */
 public class RegisterActivity extends Activity {
+	private boolean newUserDataUsable = false;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -29,20 +32,52 @@ public class RegisterActivity extends Activity {
 		getMenuInflater().inflate(R.menu.register, menu);
 		return true;
 	}
-	
+
 	/**
-	 * Sends the new client information to the server
-	 * 
+	 * Change to {@code HomeActivity} if the data could be sent to the server
 	 */
 	private OnClickListener signUpButtonListener = new View.OnClickListener() {
 		@Override
 		public void onClick(View v) {
-			submitInformationToDatabase();
+			retrieveNewUserData();
+			if (newUserDataUsable) {
+				submitUserToServer();
+				Intent displayHomeActivityIntent = new Intent(v.getContext(),
+						HomeActivity.class);
+				startActivity(displayHomeActivityIntent);
+			}
+
 		}
 	};
-	
-	private void submitInformationToDatabase() {
+
+	/**
+	 * Sends the new user's data to the server
+	 */
+	private void submitUserToServer() {
 		
+	}
+
+	/**
+	 * Package fields of the different {@code TextView} in a object that will be sent to the server
+	 * if a {@code TextView} contains badly formatted input resets the field of the {@code TextView} and sets
+	 * {@code newUserDataUsable} to false
+	 * sets {@code newUserDataUsable} to true if input is in good format
+	 */
+	private void retrieveNewUserData() {
+		final CharSequence newUserFirstName = ((TextView) findViewById(id.first_name_new_user))
+				.getText();
+		final CharSequence newUserLastName = ((TextView) findViewById(id.last_name_new_user))
+				.getText();
+		final CharSequence newUserEmail = ((TextView) findViewById(id.email_new_user))
+				.getText();
+		final CharSequence newUserPassword = ((TextView) findViewById(id.password_new_user))
+				.getText();
+		final CharSequence newUserIban = ((TextView) findViewById(id.iban_new_user))
+				.getText();
+		final CharSequence newUserBicSwift = ((TextView) findViewById(id.bic_swift_address_new_user))
+				.getText();
+		
+		newUserDataUsable = true;
 	}
 
 	@Override
