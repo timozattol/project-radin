c1924c83ff4c66f6e19cf03dab934db05d721893
Cedric Cook <cedric.cook@gmail.com>
2014-10-24 16:47:12 +0200

    Remove interface from Factory

--- ProjectRadinAndroidApp/src/ch/epfl/sweng/radin/storage/StorageManagerFactory.java
+++ ProjectRadinAndroidApp/src/ch/epfl/sweng/radin/storage/StorageManagerFactory.java
@@ -9,42 +9,8 @@ import org.json.JSONObject;
  * @author CedricCook
  *
  */
-public class StorageManagerFactory implements AbstractStorageManager {
+public class StorageManagerFactory {
 
-	/* (non-Javadoc)
-	 * @see ch.epfl.sweng.radin.storage.AbstractStorageManager#readItem(int)
-	 */
-	@Override
-	public JSONObject readItem(int id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	/* (non-Javadoc)
-	 * @see ch.epfl.sweng.radin.storage.AbstractStorageManager#createItem(org.json.JSONObject)
-	 */
-	@Override
-	public boolean createItem(JSONObject fields) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	/* (non-Javadoc)
-	 * @see ch.epfl.sweng.radin.storage.AbstractStorageManager#updateItem(org.json.JSONObject)
-	 */
-	@Override
-	public boolean updateItem(JSONObject fields) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	/* (non-Javadoc)
-	 * @see ch.epfl.sweng.radin.storage.AbstractStorageManager#deleteItem(org.json.JSONObject)
-	 */
-	@Override
-	public boolean deleteItem(JSONObject fields) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+	
 
 }
