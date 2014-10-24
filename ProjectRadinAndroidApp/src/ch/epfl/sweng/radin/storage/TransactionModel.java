720c288d3693e15585ba6c8f7c284935198ef9d0
Timothée Lottaz <timozattol@gmail.com>
2014-10-24 17:11:17 +0200

    Fix checkstyle typos and add creatorID to TransactionModel

--- ProjectRadinAndroidApp/src/ch/epfl/sweng/radin/storage/TransactionModel.java
+++ ProjectRadinAndroidApp/src/ch/epfl/sweng/radin/storage/TransactionModel.java
@@ -1,16 +1,13 @@
 package ch.epfl.sweng.radin.storage;
 
-import java.util.Arrays;
-import java.util.Currency;
 import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
 
 /**
  * @author timozattol
  * A model representing a transaction
  */
-public class TransactionModel {
+public final class TransactionModel {
     private int mTransactionID;
 
     // The id of the List containing this Transaction
@@ -25,35 +22,33 @@ public class TransactionModel {
     private Date mDate;
     private String mPurpose;
     private TransactionType mType;
+    private int creatorID;
 
     /**
-     * Constructor for TransactionModel
+     * Private constructor for TransactionModel
      */
-    public TransactionModel(int transactionID, int parentListID, int buyerID, 
+    private TransactionModel(int transactionID, int parentListID, int buyerID, 
             double amount, Currency currency, Date date, String purpose, 
             TransactionType type) {
         // Sanity checks
-        if(transactionID < 0) {
+        if (transactionID < 0) {
             throw new IllegalArgumentException("Transaction ID cannot be negative");
         }
 
-        if(parentListID < 0) {
+        if (parentListID < 0) {
             throw new IllegalArgumentException("Transaction List ID cannot be negative");
         }
 
-        if(buyerID < 0) {
+        if (buyerID < 0) {
             throw new IllegalArgumentException("Transaction ID cannot be negative");
         }
 
-        if(amount <= 0) {
-            throw new IllegalArgumentException("Transaction amount cannot be smaller " +
-            		"or equal to zero");
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Transaction amount cannot be smaller " 
+                    + "or equal to zero");
         }
-
-        if(currency == null || !TransactionModel.mAvailableCurrencies.contains(currency)) {
-            throw new IllegalArgumentException("Currency must be one of the supported currencies");
-        }
-
+        
+        mCurrency = currency;
         mTransactionID = transactionID;
         mParentListID = parentListID;
         mBuyerID = buyerID;
@@ -68,5 +63,20 @@ public class TransactionModel {
         mDate = date;
         mPurpose = purpose;
         mType = type;
+    }
+    
+    public static TransactionModel getTransactionWithID(int transactionID) {
+        
+        return null;
+    }
+    
+    public static List<TransactionModel> getTransactionsForList(int listID) {
+        
+        return null;
+    }
+    
+    public static List<TransactionModel> getTransactionsForBuyer(int buyerID) {
+        
+        return null;
     }
 }
