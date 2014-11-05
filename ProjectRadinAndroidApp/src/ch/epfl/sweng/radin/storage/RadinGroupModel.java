91d521f3fd679a6f0ba0333b27a6195ba4409f28
Simonchelbc <simon.lebail-collet@epfl.ch>
2014-11-05 11:20:05 +0100

    Add tests on setters, and tests on DateTime Chronology

--- ProjectRadinAndroidApp/src/ch/epfl/sweng/radin/storage/RadinGroupModel.java
+++ ProjectRadinAndroidApp/src/ch/epfl/sweng/radin/storage/RadinGroupModel.java
@@ -4,7 +4,8 @@ import org.joda.time.DateTime;
 
 /**
  * @author Simonchelbc
- * 
+ * RadinGroupModel is an object with only immutable fields which provides a representation of
+ * the design of our RadinGroupModel
  */
 public class RadinGroupModel {
 	private final int mRadinGroupID;
@@ -17,8 +18,9 @@ public class RadinGroupModel {
 	private String mGroupDescription;
 	private String mAvatar;
 	
-	// Set if this group is contained in another RadinGroup
+	// Set if this group is contained in another RadinGroup which ID we will set as this.mMasterID
 	private int mMasterID;
+	private boolean mHasMasterID;
 	
 
 	public RadinGroupModel(int radinGroupID, DateTime groupCreationDateTime,
@@ -37,6 +39,8 @@ public class RadinGroupModel {
 		
 		checkString("avatar", avatar);
 		mAvatar = avatar;
+		
+		mHasMasterID = false;
 	}
 
 	public RadinGroupModel(int radinGroupID, DateTime groupCreationDateTime,
@@ -46,6 +50,7 @@ public class RadinGroupModel {
 				groupDescription, avatar);
 		checkArgumentPositive("masterID", masterID);
 		mMasterID = masterID;
+		mHasMasterID = true;
 	}
 
 	private void checkArgumentPositive(String argName, double arg) {
@@ -78,6 +83,7 @@ public class RadinGroupModel {
 
 	public void setRadinGroupEndDateTime(DateTime radinGroupEndDateTime) {
 		checkArgumentNotNull("radinGroupEndDateTime", radinGroupEndDateTime);
+		checkFirstHappensBeforeSecond(mGroupCreationDateTime, radinGroupEndDateTime);
 		mRadinGroupEndDateTime = radinGroupEndDateTime;
 	}
 
@@ -87,9 +93,16 @@ public class RadinGroupModel {
 
 	public void setRadinGroupDeletionDateTime(DateTime radinGroupDeletionDateTime) {
 		checkArgumentNotNull("radinGroupDeletionDateTime", radinGroupDeletionDateTime);
+		checkFirstHappensBeforeSecond(mGroupCreationDateTime, radinGroupDeletionDateTime);
 		mRadinGroupDeletionDateTime = radinGroupDeletionDateTime;
 	}
-
+    
+	private void checkFirstHappensBeforeSecond(DateTime firstEvent, DateTime secondEvent) {
+	    if (firstEvent.isAfter(secondEvent)) {
+	        throw new IllegalArgumentException("cannot be set as wanted since first event must happen before");
+	    }
+	}
+	
 	public String getRadinGroupName() {
 		return mRadinGroupName;
 	}
@@ -116,6 +129,10 @@ public class RadinGroupModel {
 		checkString("avatar", avatar);
 		mAvatar = avatar;
 	}
+	
+	public boolean hasMasterID() {
+	    return mHasMasterID;
+	}
 
 	public int getMasterID() {
 		return mMasterID;
@@ -124,6 +141,7 @@ public class RadinGroupModel {
 	public void setMasterID(int masterID) {
 		checkArgumentPositive("masterID", masterID); 
 		mMasterID = masterID;
+		mHasMasterID = true;
 	}
 
 	public int getRadinGroupID() {
--- ProjectRadinAndroidApp/tests/src/ch/epfl/sweng/radin/test/storage/RadingGroupModelTest.java
+++ ProjectRadinAndroidApp/tests/src/ch/epfl/sweng/radin/test/storage/RadingGroupModelTest.java
@@ -10,89 +10,160 @@ import android.test.AndroidTestCase;
  * 
  */
 public class RadingGroupModelTest extends AndroidTestCase {
-	private static final int DEFAULT_RADIN_GROUP_ID = 0;
-	private static final int NEGATIVE_VALUE = -1;
-	private static final int DEFAULT_MASTER_ID = 10;
-	private static final String DEFAULT_AVATAR = "<path-to-default-avatar>";
-	private static final String DEFAULT_GROUP_NAME = "SuperRadins";
-	private static final String DEFAULT_DESCRIPTION = "Throw your debts away";
-	private static final DateTime RIGHT_NOW = DateTime.now();
-	private static final DateTime TOMORROW = RIGHT_NOW.plusDays(1);
+    private static final int NEGATIVE_VALUE = -1;
 
-	private RadinGroupModel radinGroupWithoutMaster;
-	private RadinGroupModel radinGroupWithMaster;
+    private static final int DEFAULT_RADIN_GROUP_ID = 0;
+    private static final int DEFAULT_MASTER_ID = 10;
+    private static final String DEFAULT_AVATAR = "<path-to-default-avatar>"; // TODO
+    // change
+    // when
+    // we
+    // have
+    // avatars
+    private static final String DEFAULT_GROUP_NAME = "SuperRadins";
+    private static final String DEFAULT_DESCRIPTION = "Throw your debts away";
 
-	// TODO replace by a default avatar when avatar will be defined
+    private static final int NEW_MASTER_ID = 11;
+    private static final String NEW_AVATAR = "<path-to-new-avatar>"; // TODO
+    // change
+    // when we
+    // have
+    // avatars
+    private static final String NEW_GROUP_NAME = "VeryRadins";
+    private static final String NEW_DESCRIPTION = "Throw the debters away";
+    private static final DateTime RIGHT_NOW = DateTime.now();
+    private static final DateTime YESTERDAY = RIGHT_NOW.minusDays(1);
+    private static final DateTime TOMORROW = RIGHT_NOW.plusDays(1);
+    private static final DateTime AFTER_TOMORROW = RIGHT_NOW.plusDays(2);
 
-	@Override
-	protected void setUp() throws Exception {
-		super.setUp();
-		radinGroupWithoutMaster = new RadinGroupModel(DEFAULT_RADIN_GROUP_ID,
-				RIGHT_NOW, DEFAULT_GROUP_NAME, DEFAULT_DESCRIPTION,
-				DEFAULT_AVATAR);
-		radinGroupWithMaster = new RadinGroupModel(DEFAULT_RADIN_GROUP_ID,
-				RIGHT_NOW, DEFAULT_GROUP_NAME, DEFAULT_DESCRIPTION,
-				DEFAULT_AVATAR, DEFAULT_MASTER_ID);
-	}
+    private RadinGroupModel radinGroupWithoutMaster;
+    private RadinGroupModel radinGroupWithMaster;
 
-	public void testWithoutMasterIDConstructorNullFields() {
-		try {
-			new RadinGroupModel(DEFAULT_RADIN_GROUP_ID, null, null, null, null);
-			fail("groupCreationDateTime cannot be null");
-			fail("radinGroupName cannot be null");
-			fail("groupDescription cannot be null");
-			fail("avatar cannot be null");
-		} catch (IllegalArgumentException e) {
-			// Good
-		}
-	}
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        radinGroupWithoutMaster = new RadinGroupModel(DEFAULT_RADIN_GROUP_ID,
+                RIGHT_NOW, DEFAULT_GROUP_NAME, DEFAULT_DESCRIPTION,
+                DEFAULT_AVATAR);
+        radinGroupWithMaster = new RadinGroupModel(DEFAULT_RADIN_GROUP_ID,
+                RIGHT_NOW, DEFAULT_GROUP_NAME, DEFAULT_DESCRIPTION,
+                DEFAULT_AVATAR, DEFAULT_MASTER_ID);
+    }
 
-	public void testWithoutMasterIDConstructorNegativeID() {
-		try {
-			new RadinGroupModel(NEGATIVE_VALUE, RIGHT_NOW, DEFAULT_GROUP_NAME,
-					DEFAULT_DESCRIPTION, DEFAULT_AVATAR);
-			fail("radinGroupID cannot be negative");
-		} catch (IllegalArgumentException e) {
-			// Good
-		}
-	}
+    public void testMasterID() throws Exception {
+        assertTrue(radinGroupWithMaster.hasMasterID());
+        assertFalse(radinGroupWithoutMaster.hasMasterID());
+    }
 
-	public void testWithMasterIDConstructorNullFields() {
-		try {
-			new RadinGroupModel(DEFAULT_RADIN_GROUP_ID, null, null, null, null,
-					DEFAULT_MASTER_ID);
-			fail("groupCreationDateTime cannot be null");
-			fail("radinGroupName cannot be null");
-			fail("groupDescription cannot be null");
-			fail("avatar cannot be null");
-		} catch (IllegalArgumentException e) {
-			// good
-		}
-	}
+    public void testWithoutMasterIDConstructorNullFields() {
+        try {
+            new RadinGroupModel(DEFAULT_RADIN_GROUP_ID, null, null, null, null);
+            fail("groupCreationDateTime cannot be null");
+            fail("radinGroupName cannot be null");
+            fail("groupDescription cannot be null");
+            fail("avatar cannot be null");
+        } catch (IllegalArgumentException e) {
+            assertNotNull(e);
+        }
+    }
 
-	public void testWithMasterIDConstructorNegativeIDs() {
-		try {
-			new RadinGroupModel(NEGATIVE_VALUE, RIGHT_NOW, DEFAULT_GROUP_NAME,
-					DEFAULT_DESCRIPTION, DEFAULT_AVATAR, NEGATIVE_VALUE);
-			fail("radinGroupID cannot be negative");
-			fail("masterID cannot be negative");
-		} catch (IllegalArgumentException e) {
-			// good
-		}
-	}
+    public void testWithoutMasterIDConstructorNegativeID() {
+        try {
+            new RadinGroupModel(NEGATIVE_VALUE, RIGHT_NOW, DEFAULT_GROUP_NAME,
+                    DEFAULT_DESCRIPTION, DEFAULT_AVATAR);
+            fail("radinGroupID cannot be negative");
+        } catch (IllegalArgumentException e) {
+            assertNotNull(e);
+        }
+    }
 
-	public void testConstructorsWithEmptyStringFields() {
-		try {
-			new RadinGroupModel(DEFAULT_RADIN_GROUP_ID, RIGHT_NOW, "", "", "");
-			fail("radinGroupName cannot be the empty String");
-			fail("groupDescription cannot be the empty String");
-			fail("avatar cannot be the empty String");
-			new RadinGroupModel(DEFAULT_RADIN_GROUP_ID, RIGHT_NOW, "", "", "", DEFAULT_MASTER_ID);
-			fail("radinGroupName cannot be the empty String");
-			fail("groupDescription cannot be the empty String");
-			fail("avatar cannot be the empty String");
-		} catch (IllegalArgumentException e) {
-			// good
-		}
-	}
+    public void testWithMasterIDConstructorNullFields() {
+        try {
+            new RadinGroupModel(DEFAULT_RADIN_GROUP_ID, null, null, null, null,
+                    DEFAULT_MASTER_ID);
+            fail("groupCreationDateTime cannot be null");
+            fail("radinGroupName cannot be null");
+            fail("groupDescription cannot be null");
+            fail("avatar cannot be null");
+        } catch (IllegalArgumentException e) {
+            assertNotNull(e);
+        }
+    }
+
+    public void testWithMasterIDConstructorNegativeIDs() {
+        try {
+            new RadinGroupModel(NEGATIVE_VALUE, RIGHT_NOW, DEFAULT_GROUP_NAME,
+                    DEFAULT_DESCRIPTION, DEFAULT_AVATAR, NEGATIVE_VALUE);
+            fail("radinGroupID cannot be negative");
+            fail("masterID cannot be negative");
+        } catch (IllegalArgumentException e) {
+            assertNotNull(e);
+        }
+    }
+
+    public void testConstructorsWithEmptyStringFields() {
+        try {
+            new RadinGroupModel(DEFAULT_RADIN_GROUP_ID, RIGHT_NOW, "", "", "");
+            fail("radinGroupName cannot be the empty String");
+            fail("groupDescription cannot be the empty String");
+            fail("avatar cannot be the empty String");
+            new RadinGroupModel(DEFAULT_RADIN_GROUP_ID, RIGHT_NOW, "", "", "",
+                    DEFAULT_MASTER_ID);
+            fail("radinGroupName cannot be the empty String");
+            fail("groupDescription cannot be the empty String");
+            fail("avatar cannot be the empty String");
+        } catch (IllegalArgumentException e) {
+            assertNotNull(e);
+        }
+    }
+
+    public void testModifyingValuesWithSettersWithMasterID() {
+        radinGroupWithMaster.setAvatar(NEW_AVATAR);
+        assertEquals(radinGroupWithMaster.getAvatar(),NEW_AVATAR);
+
+        radinGroupWithMaster.setGroupDescription(NEW_DESCRIPTION);
+        assertEquals(radinGroupWithMaster.getGroupDescription(),
+                NEW_DESCRIPTION);
+
+        radinGroupWithMaster.setMasterID(NEW_MASTER_ID);
+        assertEquals(radinGroupWithMaster.getMasterID(), NEW_MASTER_ID);
+        assertTrue(radinGroupWithMaster.hasMasterID());
+
+        radinGroupWithMaster.setRadinGroupEndDateTime(TOMORROW);
+        assertEquals(radinGroupWithMaster.getRadinGroupEndDateTime(),
+                TOMORROW);
+
+        radinGroupWithMaster.setRadinGroupDeletionDateTime(AFTER_TOMORROW);
+        assertEquals(radinGroupWithMaster.getRadinGroupDeletionDateTime(),
+                AFTER_TOMORROW);
+
+        radinGroupWithMaster.setRadinGroupName(NEW_GROUP_NAME);
+        assertEquals(radinGroupWithMaster.getRadinGroupName(),
+                NEW_GROUP_NAME);
+
+    }
+
+    public void testSetMasterIDToGroupWithoutMasterID() throws Exception {
+        radinGroupWithoutMaster.setMasterID(NEW_MASTER_ID);
+        assertTrue(radinGroupWithoutMaster.hasMasterID());
+        assertEquals(radinGroupWithoutMaster.getMasterID(), NEW_MASTER_ID);
+    }
+
+    public void testSetDeletionTimeHappeningBeforeCreationTime() {
+        try {
+            radinGroupWithMaster.setRadinGroupDeletionDateTime(YESTERDAY);
+            fail("cannot be set as wanted since first event must happen before");
+        } catch (IllegalArgumentException e) {
+            assertNotNull(e);
+        }
+    }
+
+    public void testSetGroupEndDateTimeHappeningBeforeCreationTime() {
+        try {
+            radinGroupWithMaster.setRadinGroupEndDateTime(YESTERDAY);
+            fail("cannot be set as wanted since first event must happen before");
+        } catch (IllegalArgumentException e) {
+            assertNotNull(e);
+        }
+    }
 }
