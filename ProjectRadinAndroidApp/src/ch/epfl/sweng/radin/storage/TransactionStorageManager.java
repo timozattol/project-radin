cd64a1aac99b3014d621b81b9ce3ddbc5debea76
Cedric Cook <cedric.cook@gmail.com>
2014-11-07 15:52:44 +0100

    Change methods signatures to take Activity reference

--- ProjectRadinAndroidApp/src/ch/epfl/sweng/radin/storage/TransactionStorageManager.java
+++ ProjectRadinAndroidApp/src/ch/epfl/sweng/radin/storage/TransactionStorageManager.java
@@ -7,64 +7,107 @@ import java.util.List;
 
 import org.json.JSONObject;
 
+import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+
 /**
  * @author CedricCook
  *
  */
-public class TransactionStorageManager implements StorageManager {
+public class TransactionStorageManager implements StorageManager<TransactionModel> {
+	TransactionStorageManager transactionStorageManager;
+	private Parser parser = new TransactionParser();
+	
+	private TransactionStorageManager() {
+		
+	}
 
 	/* (non-Javadoc)
 	 * @see ch.epfl.sweng.radin.storage.StorageManager#getStorageManager()
 	 */
 	@Override
-	public StorageManager getStorageManager() {
+	public StorageManager<TransactionModel> getStorageManager() {
+		if(transactionStorageManager == null) {
+			transactionStorageManager = new TransactionStorageManager();
+		}
+		return transactionStorageManager;
+	}
+
+	/* (non-Javadoc)
+	 * @see ch.epfl.sweng.radin.storage.StorageManager#getById(int, android.app.Activity)
+	 */
+	@Override
+	public TransactionModel getById(int id, Activity caller) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	/* (non-Javadoc)
-	 * @see ch.epfl.sweng.radin.storage.StorageManager#getById(int)
+	 * @see ch.epfl.sweng.radin.storage.StorageManager#getAll(android.app.Activity)
 	 */
 	@Override
-	public JSONObject getById(int id) {
+	public List<TransactionModel> getAll(Activity caller) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	/* (non-Javadoc)
-	 * @see ch.epfl.sweng.radin.storage.StorageManager#getAll()
+	 * @see ch.epfl.sweng.radin.storage.StorageManager#create(java.util.List, android.app.Activity)
 	 */
 	@Override
-	public List<JSONObject> getAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	/* (non-Javadoc)
-	 * @see ch.epfl.sweng.radin.storage.StorageManager#create(java.util.List)
-	 */
-	@Override
-	public boolean create(List<JSONObject> entries) {
+	public boolean create(List<TransactionModel> entries, Activity caller) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	/* (non-Javadoc)
-	 * @see ch.epfl.sweng.radin.storage.StorageManager#update(java.util.List)
+	 * @see ch.epfl.sweng.radin.storage.StorageManager#update(java.util.List, android.app.Activity)
 	 */
 	@Override
-	public boolean update(List<JSONObject> entries) {
+	public boolean update(List<TransactionModel> entries, Activity caller) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	/* (non-Javadoc)
-	 * @see ch.epfl.sweng.radin.storage.StorageManager#delete(java.util.List)
+	 * @see ch.epfl.sweng.radin.storage.StorageManager#delete(java.util.List, android.app.Activity)
 	 */
 	@Override
-	public boolean delete(List<JSONObject> entries) {
+	public boolean delete(List<TransactionModel> entries, Activity caller) {
 		// TODO Auto-generated method stub
 		return false;
+	}
+	
+	private boolean isConnected() {
+		ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+		return (networkInfo != null && networkInfo.isConnected());
+	}
+	
+	private class TransactionConnectionTask extends AsyncTask<String, Void, String> {
+
+		/* (non-Javadoc)
+		 * @see android.os.AsyncTask#doInBackground(Params[])
+		 */
+		@Override
+		protected String doInBackground(String... params) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+		/* (non-Javadoc)
+		 * @see android.os.AsyncTask#onPostExecute(java.lang.Object)
+		 */
+		@Override
+		protected void onPostExecute(String result) {
+			// TODO Auto-generated method stub
+			super.onPostExecute(result);
+		}
+		
 	}
 
 }
