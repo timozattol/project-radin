ffc9b0fdc01398a1d25c4c5069c726cb5de16925
Cedric Cook <cedric.cook@gmail.com>
2014-11-10 23:43:57 +0100

    Create the doInBackground & onPostExecute methods

--- ProjectRadinAndroidApp/src/ch/epfl/sweng/radin/storage/RadinGroupStorageManager.java
+++ ProjectRadinAndroidApp/src/ch/epfl/sweng/radin/storage/RadinGroupStorageManager.java
@@ -3,8 +3,16 @@
  */
 package ch.epfl.sweng.radin.storage;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.app.Activity;
 import android.content.Context;
@@ -40,8 +48,8 @@ public class RadinGroupStorageManager implements StorageManager<RadinGroupModel>
 	 */
 	@Override
 	public RadinGroupModel getById(int id, RadinListener callback) {
-		// TODO Auto-generated method stub
-		return null;
+		RadinGroupConnectionTask connTask = new RadinGroupConnectionTask(callback);
+		connTask.execute()
 	}
 
 	/* (non-Javadoc)
@@ -93,14 +101,39 @@ public class RadinGroupStorageManager implements StorageManager<RadinGroupModel>
 	
 	private class RadinGroupConnectionTask extends AsyncTask<String, Void, String> {
 
+		private RadinListener mListener;
+		
+		public RadinGroupConnectionTask(RadinListener listener) {
+			mListener = listener;
+		}
+		
 		/* (non-Javadoc)
 		 * @see android.os.AsyncTask#doInBackground(Params[])
 		 */
 		@Override
 		protected String doInBackground(String... params) {
-			URL url = new URL(params[0]);
-			String requestMethod = params[1];
-			String jsonParams = params[2];
+			try {
+				URL url = new URL(params[0]);
+				String requestMethod = params[1];
+				String jsonParams = params[2];
+				
+				//Database AND/OR server:
+				// Very rough pseudo code! must see with server and database for specifics.
+				Database db = new Database();
+				if(requestMethod.equals("GET")) {
+					db.select(jsonParams);
+				}
+				
+				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+	            conn.setRequestMethod(requestMethod);
+	            conn.setDoInput(true);
+	            conn.connect();
+	            
+	            return fetchContent(conn);
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		
 		/* (non-Javadoc)
@@ -108,9 +141,36 @@ public class RadinGroupStorageManager implements StorageManager<RadinGroupModel>
 		 */
 		@Override
 		protected void onPostExecute(String result) {
-			// TODO Auto-generated method stub
-			super.onPostExecute(result);
+			JSONParser jsonParser = new RadinGroupJSONParser();
+			//TODO check that result is valid json
+			JSONObject json;
+			try {
+				json = new JSONObject(result);
+				
+				//HACK because the parser takes a list, should it take just one object instead?
+				List<JSONObject> jsonList = new ArrayList<JSONObject>();
+				jsonList.add(json);
+				List<Model> models = jsonParser.getModelsFromJson(jsonList);
+				
+				mListener.callback(models);
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+
 		}
+		
+	    private String fetchContent(HttpURLConnection conn) throws IOException {
+	        // Credits go to http://stackoverflow.com/questions/309424/read-convert-an-inputstream-to-a-string
+	        InputStream is = conn.getInputStream();
+	        try {
+	            java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+	            return s.hasNext() ? s.next() : "";
+	        } finally {
+	            is.close();
+	        }
+	    }
 		
 	}
 
