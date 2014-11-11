46fba55c05652310245b8e6f1fab2f247cfb6d36
Simonchelbc <simon.lebail-collet@epfl.ch>
2014-11-11 08:01:51 +0100

    Add enum to radinGroupTableHelper

--- ProjectRadinAndroidApp/src/ch/epfl/sweng/radin/databases/RadinGroupTableHelper.java
+++ ProjectRadinAndroidApp/src/ch/epfl/sweng/radin/databases/RadinGroupTableHelper.java
@@ -7,23 +7,60 @@ import android.database.sqlite.SQLiteDatabase;
  *         database
  */
 public class RadinGroupTableHelper implements TableHelper {
+	/**
+	 * @author Simonchelbc
+	 *
+	 */
+	public enum Column {// TODO problem letting it public might
+		// be bad for privacy but we need it
+		// for test.databases so far
+		RID("_RID"), RG_NAME("RG_NAME"), RG_CREATION_DATE("RG_CREATION_DATE"), RG_DESCRIPTION(
+				"RG_DESCRIPTION"), RG_GROUP("RG_GROUP"), RG_MASTER_RID(
+				"RG_MASTER_RID"), RG_AVATAR("RG_AVATAR"), RG_DELETED_AT(
+				"RG_DELETED_AT"), RG_END_DATE("RG_END_DATE");
+
+		private final String getSqlName; 
+
+		private Column(String sqlName) {
+			getSqlName = sqlName;
+		}
+		
+		public String getSqlName() {
+			return getSqlName;
+		}
+	}
+
 	public static final String TABLE_RADIN_GROUP = "RADIN_GROUP";
-	public static final String RID = "_RID";
-	public static final String RG_NAME = "RG_NAME";
-	public static final String RG_CREATION_DATE = "RG_CREATION_DATE";
-	public static final String RG_DESCRIPTION = "RG_DESCRIPTION";
-	public static final String RG_GROUP = "RG_GROUP";
-	public static final String RG_MASTER_RID = "RG_MASTER_RID";
-	public static final String RG_AVATAR = "RG_AVATAR TEXT";
-	public static final String RG_END_DATE = "RG_END_DATE";
-	public static final String RG_DELETED_AT = "RG_DELETED_AT";
 
 	private static final String CREATE_TABLE_RADIN_GROUP = "CREATE TABLE "
-			+ TABLE_RADIN_GROUP + "(" + RID + " INT NOT NULL," + RG_NAME
-			+ " TEXT NOT NULL," + RG_CREATION_DATE + "TEXT NOT NULL,"
-			+ RG_DESCRIPTION + "TEXT," + RG_GROUP + "TEXT," + RG_MASTER_RID
-			+ "," + RG_AVATAR + "TEXT ," + RG_END_DATE + "TEXT,"
-			+ RG_DELETED_AT + "TEXT ," + "PRIMARY KEY ( " + RID + " ));";
+			+ TABLE_RADIN_GROUP + "(" + Column.RID.getSqlName + " INT NOT NULL,"
+			+ Column.RG_NAME.getSqlName + " TEXT NOT NULL,"
+			+ Column.RG_CREATION_DATE.getSqlName + " TEXT NOT NULL,"
+			+ Column.RG_DESCRIPTION.getSqlName + " TEXT,"
+			+ Column.RG_GROUP.getSqlName + " TEXT,"
+			+ Column.RG_MASTER_RID.getSqlName + "," + Column.RG_AVATAR.getSqlName
+			+ " TEXT," + Column.RG_END_DATE.getSqlName + " TEXT,"
+			+ Column.RG_DELETED_AT.getSqlName + " TEXT," + "PRIMARY KEY ("
+			+ Column.RID.getSqlName + "));";
+
+//	private final String[] mNames = new String[Column.values().length]; //TODO possibility to make it look like a 
+	//lazy val?
+
+	/**
+	 * @return array of all columns names as {@link String}, thus an array of
+	 *         {@code mNames}
+	 */
+
+	public synchronized static String[] names() {
+		final Column[] values = Column.values();
+		final int enumSize = values.length;
+		final String[] names = new String[enumSize];
+		for (int i = 0; i < enumSize; i++) {
+			names[i] = values[i].getSqlName;
+		}
+		return names;
+	}
+
 
 	public void onCreate(SQLiteDatabase db) {
 		db.execSQL(CREATE_TABLE_RADIN_GROUP);
