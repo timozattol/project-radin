4ddfa11aae5a5026bc7797b5edebeb34da59b608
Simonchelbc <simon.lebail-collet@epfl.ch>
2014-11-14 00:03:15 +0100

    Wrap RG_group related fields into comments

--- ProjectRadinAndroidApp/src/ch/epfl/sweng/radin/databases/RadinGroupTableHelper.java
+++ ProjectRadinAndroidApp/src/ch/epfl/sweng/radin/databases/RadinGroupTableHelper.java
@@ -1,12 +1,13 @@
 package ch.epfl.sweng.radin.databases;
 
+import ch.epfl.sweng.radin.storage.RadinGroupModel;
 import android.database.sqlite.SQLiteDatabase;
 
 /**
  * @author Simonchelbc Used to create the table associated to Radin Group of our
  *         database
  */
-public class RadinGroupTableHelper implements TableHelper {
+public final class RadinGroupTableHelper implements TableHelper {
 	/**
 	 * @author Simonchelbc
 	 *
@@ -15,9 +16,9 @@ public class RadinGroupTableHelper implements TableHelper {
 		// be bad for privacy but we need it
 		// for test.databases so far
 		RID("_RID"), RG_NAME("RG_NAME"), RG_CREATION_DATE("RG_CREATION_DATE"), RG_DESCRIPTION(
-				"RG_DESCRIPTION"), RG_GROUP("RG_GROUP"), RG_MASTER_RID(
+				"RG_DESCRIPTION"),  RG_MASTER_RID(
 				"RG_MASTER_RID"), RG_AVATAR("RG_AVATAR"), RG_DELETED_AT(
-				"RG_DELETED_AT"), RG_END_DATE("RG_END_DATE");
+				"RG_DELETED_AT"), RG_END_DATE("RG_END_DATE"); //RG_GROUP("RG_GROUP"), TODO #radinGroup
 
 		private final String getSqlName; 
 
@@ -37,7 +38,7 @@ public class RadinGroupTableHelper implements TableHelper {
 			+ Column.RG_NAME.getSqlName + " TEXT NOT NULL,"
 			+ Column.RG_CREATION_DATE.getSqlName + " TEXT NOT NULL,"
 			+ Column.RG_DESCRIPTION.getSqlName + " TEXT,"
-			+ Column.RG_GROUP.getSqlName + " TEXT,"
+//			+ Column.RG_GROUP.getSqlName + " TEXT," //TODO #rgGroup
 			+ Column.RG_MASTER_RID.getSqlName + "," + Column.RG_AVATAR.getSqlName
 			+ " TEXT," + Column.RG_END_DATE.getSqlName + " TEXT,"
 			+ Column.RG_DELETED_AT.getSqlName + " TEXT," + "PRIMARY KEY ("
@@ -50,7 +51,6 @@ public class RadinGroupTableHelper implements TableHelper {
 	 * @return array of all columns names as {@link String}, thus an array of
 	 *         {@code mNames}
 	 */
-
 	public synchronized static String[] names() {
 		final Column[] values = Column.values();
 		final int enumSize = values.length;
@@ -69,7 +69,6 @@ public class RadinGroupTableHelper implements TableHelper {
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 		// TODO if we need to get previous data from old database
 		// and put them in a new Database schema
-
 	}
 
 }
