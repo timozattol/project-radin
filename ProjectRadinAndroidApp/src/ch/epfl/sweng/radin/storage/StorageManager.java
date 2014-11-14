f91a7c06594f58b3ee874198d987058d94be07df
Fabien Zellweger <fabien.zellweger@epfl.ch>
2014-11-14 17:12:24 +0100

    Implement method in the storageManager

--- ProjectRadinAndroidApp/src/ch/epfl/sweng/radin/storage/StorageManager.java
+++ ProjectRadinAndroidApp/src/ch/epfl/sweng/radin/storage/StorageManager.java
@@ -24,13 +24,13 @@ import ch.epfl.sweng.radin.callback.RadinListener;
  * @param <M> the type of Model handled by the StorageManager
  */
 public abstract class StorageManager<M extends Model> {
-    
+
 	private static Context context = null;
 	static final String SERVER_BASE_URL = "radin.epfl.ch/";
 	private JSONParser<M> mJsonParser = null;
-	
+
 	public abstract StorageManager<M> getStorageManager();
-	
+
 	/**
 	 * Meant to be overridden by child classes, to fill jsonParser with a new 
 	 * type-specific parser if empty, then return the mJsonParser
@@ -43,36 +43,48 @@ public abstract class StorageManager<M extends Model> {
 	 * For example "user", or "radinGroup"
 	 * @return the type url.
 	 */
-    protected abstract String getTypeUrl();
-	
+	protected abstract String getTypeUrl();
+
 	/**
 	 * Initiates the StorageManager with the application Context
 	 * The Context is needed when we check the connection
 	 * @param appContext the context of the Application
 	 */
 	public static void init(Context appContext) {
-	    if (context == null) {
-	        context = appContext;
-	    }
+		if (context == null) {
+			context = appContext;
+		}
 	}
-	
+
 	/* (non-Javadoc)
 	 * @see ch.epfl.sweng.radin.storage.StorageManager#getById(int, android.app.Activity)
 	 */
 	public boolean getById(int id, RadinListener<M> callback) {
-		GeneralConnectionTask connTask = new GeneralConnectionTask(callback);
-		connTask.execute(SERVER_BASE_URL + getTypeUrl(), "GET", String.valueOf(id));
-		
+		if (isConnected()) {
+			if (!isHashMatchServer()) {
+				GeneralConnectionTask connTask = new GeneralConnectionTask(callback);
+				connTask.execute(SERVER_BASE_URL + getTypeUrl() + "/" + String.valueOf(id), "GET");
+			}
+		}
+		//TODO take the data from the local DB		
 		//TODO make this value represent something.
 		return true;
 	}
-	
+
 	/* (non-Javadoc)
 	 * @see ch.epfl.sweng.radin.storage.StorageManager#getAll(android.app.Activity)
 	 */
 	public boolean getAll(RadinListener<M> callback) {
+		if (isConnected()) {
+			if (!isHashMatchServer()) {
+				GeneralConnectionTask connTask = new GeneralConnectionTask(callback);
+				connTask.execute(SERVER_BASE_URL + getTypeUrl(), "GET");
+				return true;
+			}
+		}
+		//TODO take the data from the local DB
 		//TODO make this value represent something.
-		return false;
+		return true;
 	}
 
 	/* (non-Javadoc)
@@ -80,24 +92,39 @@ public abstract class StorageManager<M extends Model> {
 	 */
 
 	public boolean create(List<M> entries, RadinListener<M> callback) {
-		//TODO make this value represent something.
-		return false;
+		if (isConnected()){
+			GeneralConnectionTask connTask = new GeneralConnectionTask(callback);
+			JSONObject json = (JSONObject) getJSONParser().getJsonFromModels(entries);
+			connTask.execute(SERVER_BASE_URL + getTypeUrl(), "POST", json.toString());
+		}
+		//TODO set the data in the local DB
+		return true;
 	}
-	
+
 	/* (non-Javadoc)
 	 * @see ch.epfl.sweng.radin.storage.StorageManager#update(java.util.List, android.app.Activity)
 	 */
 	public boolean update(List<M> entries, RadinListener<M> callback) {
-		// TODO make this value represent something.
-		return false;
+		if (isConnected()){
+			GeneralConnectionTask connTask = new GeneralConnectionTask(callback);
+			JSONObject json = (JSONObject) getJSONParser().getJsonFromModels(entries);
+			connTask.execute(SERVER_BASE_URL + getTypeUrl(), "PUT", json.toString());
+		}
+		//TODO modify the data in the local DB
+		return true;
 	}
-	
+
 	/* (non-Javadoc)
 	 * @see ch.epfl.sweng.radin.storage.StorageManager#delete(java.util.List, android.app.Activity)
 	 */
 	public boolean delete(List<M> entries, RadinListener<M> callback) {
-		// TODO Auto-generated method stub
-		return false;
+		if (isConnected()){
+			GeneralConnectionTask connTask = new GeneralConnectionTask(callback);
+			JSONObject json = (JSONObject) getJSONParser().getJsonFromModels(entries);
+			connTask.execute(SERVER_BASE_URL + getTypeUrl(), "DELETE", json.toString());
+		}
+		//TODO delet the data in the local DB
+		return true;
 	}
 
 	private boolean isConnected() {
@@ -106,15 +133,20 @@ public abstract class StorageManager<M extends Model> {
 
 		return networkInfo != null && networkInfo.isConnected();
 	}
+	
+	private boolean isHashMatchServer(){
+		// TODO Creat a methode who verify our hash match the one from the server
+		return true;
+	}
 
 	private class GeneralConnectionTask extends AsyncTask<String, Void, String> {
 
 		private RadinListener mListener;
-		
+
 		public GeneralConnectionTask(RadinListener listener) {
 			mListener = listener;
 		}
-		
+
 		/* (non-Javadoc)
 		 * @see android.os.AsyncTask#doInBackground(Params[])
 		 */
@@ -124,26 +156,26 @@ public abstract class StorageManager<M extends Model> {
 				URL url = new URL(params[0]);
 				String requestMethod = params[1];
 				String jsonParams = params[2];
-				
+
 				//Database AND/OR server:
 				// Very rough pseudo code! must see with server and database for specifics.
 				Database db = new Database();
 				if(requestMethod.equals("GET")) {
 					db.select(jsonParams);
 				}
-				
+
 				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-	            conn.setRequestMethod(requestMethod);
-	            conn.setDoInput(true);
-	            conn.connect();
-	            
-	            return fetchContent(conn);
+				conn.setRequestMethod(requestMethod);
+				conn.setDoInput(true);
+				conn.connect();
+
+				return fetchContent(conn);
 			} catch (MalformedURLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
-		
+
 		/* (non-Javadoc)
 		 * @see android.os.AsyncTask#onPostExecute(java.lang.Object)
 		 */
@@ -153,30 +185,30 @@ public abstract class StorageManager<M extends Model> {
 			JSONObject json;
 			try {
 				json = new JSONObject(result);
-				
+
 				//HACK because the parser takes a list, should it take just one object instead?
 				List<JSONObject> jsonList = new ArrayList<JSONObject>();
 				jsonList.add(json);
 				List<Model> models = mJsonParser.getModelsFromJson(jsonList);
-				
+
 				mListener.callback(models);
 			} catch (JSONException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
-		
-	    private String fetchContent(HttpURLConnection conn) throws IOException {
-	        // Credits go to http://stackoverflow.com/questions/309424/read-convert-an-inputstream-to-a-string
-	        InputStream is = conn.getInputStream();
-	        try {
-	            java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
-	            return s.hasNext() ? s.next() : "";
-	        } finally {
-	            is.close();
-	        }
-	    }
-		
+
+		private String fetchContent(HttpURLConnection conn) throws IOException {
+			// Credits go to http://stackoverflow.com/questions/309424/read-convert-an-inputstream-to-a-string
+			InputStream is = conn.getInputStream();
+			try {
+				java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+				return s.hasNext() ? s.next() : "";
+			} finally {
+				is.close();
+			}
+		}
+
 	}
-	
+
 }
