a35ea5f82288cc83f75b5646b6b84417fe5884b7
Simonchelbc <simon.lebail-collet@epfl.ch>
2014-11-15 15:51:11 +0100

    Add scrollTo usage in an attempt to make test work

--- ProjectRadinAndroidApp/tests/src/ch/epfl/sweng/radin/test/databases/DatabaseRadinGroupTableActivityEspressoTest.java
+++ ProjectRadinAndroidApp/tests/src/ch/epfl/sweng/radin/test/databases/DatabaseRadinGroupTableActivityEspressoTest.java
@@ -1,19 +1,15 @@
 package ch.epfl.sweng.radin.test.databases;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
-import org.joda.time.DateTime;
+import com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.Visibility;
 
-import ch.epfl.sweng.radin.databases.Database;
-import ch.epfl.sweng.radin.databases.RadinGroupTableHelper;
-import ch.epfl.sweng.radin.storage.RadinGroupModel;
-import ch.epfl.sweng.radin.test.R;
-
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.test.ActivityInstrumentationTestCase2;
+import android.util.Log;
 import android.widget.TextView;
+import ch.epfl.sweng.radin.DatabaseRadinGroupTableActivity;
+import ch.epfl.sweng.radin.R;
 
 import static com.google.android.apps.common.testing.ui.espresso.Espresso.onData;
 import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView;
@@ -21,10 +17,12 @@ import static com.google.android.apps.common.testing.ui.espresso.Espresso.pressB
 import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.click;
 import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.closeSoftKeyboard;
 import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.typeText;
+import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.scrollTo;
 import static com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions.matches;
 import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withId;
-import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withText;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.isEnabled;
+
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.instanceOf;
@@ -41,94 +39,46 @@ import static org.hamcrest.Matchers.not;
 
 public class DatabaseRadinGroupTableActivityEspressoTest extends
 		ActivityInstrumentationTestCase2<DatabaseRadinGroupTableActivity> {
+	private static final String TAG = "DatabaseRadinGroupTableActivityEspressoTest";
+	private static final int SLEEP_TIME = 100;
+	private DatabaseRadinGroupTableActivity mRadinGroupActivity;
 
-	public DatabaseRadinGroupTableActivityEspressoTest(
-			Class<DatabaseRadinGroupTableActivity> activityClass) {
-		super(activityClass);
-		getActivity();
+	public DatabaseRadinGroupTableActivityEspressoTest() {
+		super(DatabaseRadinGroupTableActivity.class);
 	}
 
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
-
+		mRadinGroupActivity = getActivity();
 	}
 
 	private void typeTextInField(int rId, String text) {
-		onView(withId(rId)).perform(typeText(text));
+		onView(withId(rId)).perform(scrollTo(), typeText(text));
 	}
-
-	public void testGoodContentValueWithFields() throws Exception {
-		typeTextInField(R.id.RGAvatar, "0");
-		typeTextInField(R.id.RGCreationDate, "today");
-		typeTextInField(R.id.RGDescription, "A cool group");
-		typeTextInField(R.id.RGGroup, "group radin");
-		typeTextInField(R.id.RGMasterID, "10");
-		typeTextInField(R.id.RGAvatar, "img/avatar1.png");
-		typeTextInField(R.id.RGEndedAt, "tomorrow");
-		typeTextInField(R.id.RGDeletedAt, "after-tomorrow");
-		onView(withId(R.id.submitRadinGroupToDB)).perform(click());
-	}
-
-	private List<RadinGroupModel> getAllModels(SQLiteDatabase db) {
-		List<RadinGroupModel> rgs = new ArrayList<>();
-		Cursor cursor = db.query(RadinGroupTableHelper.TABLE_RADIN_GROUP,
-				RadinGroupTableHelper.names(), null, null, null, null, null);
-		cursor.moveToFirst();
-		while (!cursor.isAfterLast()) {
-			rgs.add(cursorToRadinGroupModel(cursor));
-		}
-		cursor.close();
-		return rgs;
-	}
-
-	private RadinGroupModel cursorToRadinGroupModel(Cursor cursor) {
-//		String[] columns = cursor.getColumnNames();
-//		for(String col: columns) {
-//			
-//		}
-		int rid = cursor.getColumnIndex(RadinGroupTableHelper.Column.RID
-				.getSqlName());
-
-		int RGavatar = cursor
-				.getColumnIndex(RadinGroupTableHelper.Column.RG_AVATAR
-						.getSqlName());
-
-		int rgCreation = cursor
-				.getColumnIndex(RadinGroupTableHelper.Column.RG_CREATION_DATE
-						.getSqlName());
-
-		int rgDeleted = cursor
-				.getColumnIndex(RadinGroupTableHelper.Column.RG_DELETED_AT
-						.getSqlName());
-
-		int rgEndDate = cursor
-				.getColumnIndex(RadinGroupTableHelper.Column.RG_END_DATE
-						.getSqlName());
-
-		int rgDescr = cursor
-				.getColumnIndex(RadinGroupTableHelper.Column.RG_DESCRIPTION
-						.getSqlName());
-
-//		int rgGroup = cursor
-//				.getColumnIndex(RadinGroupTableHelper.Column.RG_GROUP
-//						.getSqlName()); //TODO #rgGroup
-
-		int rgMasterId = cursor
-				.getColumnIndex(RadinGroupTableHelper.Column.RG_MASTER_RID
-						.getSqlName());
-
-		int rgName = cursor.getColumnIndex(RadinGroupTableHelper.Column.RG_NAME
-				.getSqlName());
-		
-
-		cursor.getString(rgCreation);
-		cursor.getString(rgDeleted);
-		cursor.getString(rgEndDate);
-		RadinGroupModel rgModel = new RadinGroupModel(rid, null, null, null,
-				null, 0);
-		return rgModel;
-	}
-
 	
+	public void testDBAfterInsert() throws Exception {
+		typeTextInField(R.id.RID, "0");
+		typeTextInField(R.id.RGCreatedAt, "today");
+		typeTextInField(R.id.RGDeletedAt, "after-tomorrow");
+		typeTextInField(R.id.RGEndedAt, "tomorrow");
+		typeTextInField(R.id.RGName, "test group");
+		typeTextInField(R.id.RGAvatar, "img/avatar1.png");
+		typeTextInField(R.id.RGMasterID, "10");
+		typeTextInField(R.id.RGDescription, "A cool group");
+		closeSoftKeyboard();
+		
+		Log.v(TAG, "performing click on submitRadinGroupToDB");
+		onView(withId(R.id.submitRadinGroupToDB)).perform(scrollTo());
+		assertTrue(withEffectiveVisibility(Visibility.VISIBLE).matches(withId(R.id.submitRadinGroupToDB)));
+		onView(allOf(withId(R.id.submitRadinGroupToDB))).perform(scrollTo(), click());
+		
+		List<Map<String, String>> rows = mRadinGroupActivity
+				.getEverythingFromRadinGroupTable();
+		assertEquals(rows.size(), 1);
+		Map<String, String> uniqueRow = rows.get(0);
+		uniqueRow.get(0);
+
+	}
+
 }
