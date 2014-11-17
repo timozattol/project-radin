c13899f799e14f704aeacf0ce6e8827d381ca2e5
Ireneu Pla <ireneu.pla@epfl.ch>
2014-11-17 23:41:02 +0100

    test change on RadinGroup DB construction for better built-in error and type checking

--- radin-server/app/DB/Tables.scala
+++ radin-server/app/DB/Tables.scala
@@ -1,23 +1,16 @@
 package DB
 
-import scala.slick.driver.H2Driver.simple._
+//import scala.slick.driver.H2Driver.simple._
 import scala.slick.lifted.{ ProvenShape, ForeignKeyQuery }
+import scala.slick.driver.SQLiteDriver.simple._
+
+import play.api.mvc.BodyParsers._
+import play.api.libs.json.Json
+import play.api.libs.json.Json._
 
 class Tables {
 
-  class RadinGroup(tag: Tag) extends Table[(Int, String, String, String, Int, String, String, String)](tag, "RADINGROUP") {
-    def rid: Column[Int] = column[Int]("RID", O.PrimaryKey, O.AutoInc, O.NotNull)
-    def RGname: Column[String] = column[String]("RG_NAME", O.NotNull)
-    def RGstartDate: Column[String] = column[String]("RG_STARTDATE", O.NotNull)
-    def RGdescription: Column[String] = column[String]("RG_DESCRIPTION")
-    def RGmasterRID: Column[Int] = column[Int]("RG_MASTERRID")
-    def RGavatar: Column[String] = column[String]("RG_AVATAR")
-    def RGendDate: Column[String] = column[String]("RG_ENDDATE")
-    def RGdeletedAt: Column[String] = column[String]("RG_DELETEDAT")
-
-    def * = (rid, RGname, RGstartDate, RGdescription, RGmasterRID, RGavatar, RGendDate, RGdeletedAt)
-  }
-  lazy val radinGroups = TableQuery[RadinGroup]
+  val radinGroups = TableQuery[RadinGroups]
 
   class Transaction(tag: Tag) extends Table[(Int, Int, Int, Int, Int, String, Int, String, Int, String, Int, String)](tag, "TRANSACTION") {
 
@@ -67,80 +60,95 @@ class Tables {
     def * = (uidSource, uidTarget, URrelation)
   }
   lazy val userRelationships = TableQuery[UserRelationship]
-  
+
   class MemberInRadin(tag: Tag) extends Table[(Int, Int, String, Int, String)](tag, "MEMBER_IN_RADIN") {
-      
-      def MRuid: Column[Int] = column[Int]("UID", O.NotNull)
-      def MRrid: Column[Int] = column[Int]("RID", O.NotNull)
-      def MRcreationDate: Column[String] = column[String]("MR_CREATION_DATE", O.NotNull)
-      def MRpermission: Column[Int] = column[Int]("MR_PERMISSION", O.NotNull)
-      def MRdeletedAt: Column[String] = column[String]("MR_DELETED_AT")
-      
-      def memberUser = foreignKey("MEMBER_USER", MRuid, users)(_.uid)
-      def memberRadinGroup = foreignKey("MEMBER_RG", MRrid, radinGroups)(_.rid)
-      def MRpk = primaryKey("MRpk", (MRuid, MRrid))
-      
-      def * = (MRuid, MRrid, MRcreationDate, MRpermission, MRdeletedAt)   
+
+    def MRuid: Column[Int] = column[Int]("UID", O.NotNull)
+    def MRrid: Column[Int] = column[Int]("RID", O.NotNull)
+    def MRcreationDate: Column[String] = column[String]("MR_CREATION_DATE", O.NotNull)
+    def MRpermission: Column[Int] = column[Int]("MR_PERMISSION", O.NotNull)
+    def MRdeletedAt: Column[String] = column[String]("MR_DELETED_AT")
+
+    def memberUser = foreignKey("MEMBER_USER", MRuid, users)(_.uid)
+    def memberRadinGroup = foreignKey("MEMBER_RG", MRrid, radinGroups)(_.rid)
+    def MRpk = primaryKey("MRpk", (MRuid, MRrid))
+
+    def * = (MRuid, MRrid, MRcreationDate, MRpermission, MRdeletedAt)
   }
   lazy val memberInRadins = TableQuery[MemberInRadin]
-  
+
   class GroupPreset(tag: Tag) extends Table[(Int, Int, String, String)](tag, "GROUP_PRESET") {
-    
+
     def GPgid: Column[Int] = column[Int]("GID", O.PrimaryKey, O.NotNull)
     def GPrid: Column[Int] = column[Int]("RID", O.NotNull)
     def GPname: Column[String] = column[String]("GP_NAME", O.NotNull)
     def GPdeletedAt: Column[String] = column[String]("GP_DELETED_AT")
-    
+
     def presetRadinGroup = foreignKey("PRESET_RG", GPrid, radinGroups)(_.rid)
-    
+
     def * = (GPgid, GPrid, GPname, GPdeletedAt)
   }
   lazy val groupPresets = TableQuery[GroupPreset]
-  
+
   class MemberInGroup(tag: Tag) extends Table[(Int, Int, String)](tag, "MEMBER_IN_GROUP") {
-    
+
     def MGgid: Column[Int] = column[Int]("GID", O.NotNull)
     def MGuid: Column[Int] = column[Int]("UID", O.NotNull)
     def MGdeletedAt: Column[String] = column[String]("MB_DELETED_AT")
-    
+
     def groupPreset = foreignKey("GROUP_GP", MGgid, groupPresets)(_.GPgid)
     def groupUser = foreignKey("GROUP_U", MGuid, users)(_.uid)
     def MGpk = primaryKey("MGpk", (MGgid, MGuid))
-    
+
     def * = (MGgid, MGuid, MGdeletedAt)
   }
   lazy val memberInGroups = TableQuery[MemberInGroup]
-  
+
   class Invitation(tag: Tag) extends Table[(Int, Int, String, String, Int, String)](tag, "INVITATION") {
-    
+
     def Iuid: Column[Int] = column[Int]("UID", O.NotNull)
     def Irid: Column[Int] = column[Int]("RID", O.NotNull)
     def IstartDate: Column[String] = column[String]("I_START_DATE", O.NotNull)
     def IacceptedDate: Column[String] = column[String]("I_ACCEPTED_DATE")
     def Itargetuid: Column[Int] = column[Int]("I_TARGET_UID", O.NotNull)
     def IdeletedAt: Column[String] = column[String]("I_DELETED_AT")
-    
+
     def invitationUser = foreignKey("INVITATION_U", Iuid, users)(_.uid)
     def invitationRadinGroup = foreignKey("INVITATION_RG", Irid, radinGroups)(_.rid)
     def invitationTarget = foreignKey("INTIVATION_TU", Itargetuid, users)(_.uid)
     def Ipk = primaryKey("Ipk", (Iuid, Irid))
-    
+
     def * = (Iuid, Irid, IstartDate, IacceptedDate, Itargetuid, IdeletedAt)
   }
   lazy val invitations = TableQuery[Invitation]
-  
+
   class UserConcernedByTransaction(tag: Tag) extends Table[(Int, Int, Int)](tag, "USER_CONCERNED_BY_TRANSACTION") {
-    
+
     def UCtid: Column[Int] = column[Int]("TID", O.NotNull)
     def UCuid: Column[Int] = column[Int]("UID", O.NotNull)
     def UCcoefficient: Column[Int] = column[Int]("UC_COEFFICIENT", O.NotNull)
-    
+
     def concernsTransaction = foreignKey("CONCERNED_TID", UCtid, transactions)(_.tid)
     def concernsUser = foreignKey("CONCERNED_UID", UCuid, users)(_.uid)
     def UCpk = primaryKey("UCpk", (UCtid, UCuid))
-    
+
     def * = (UCtid, UCuid, UCcoefficient)
   }
   lazy val userConcernedByTransactions = TableQuery[UserConcernedByTransaction]
-  
+
+}
+
+case class RadinGroup(RGname: String, RGstartDate: String, RGdescription: String, RGmasterID: Int, RGavatar: String, RGendDate: String, RGdeletedAt: String, id: Option[Int] = None)
+
+class RadinGroups(tag: Tag) extends Table[RadinGroup](tag, "RADINGROUP") {
+  def rid: Column[Int] = column[Int]("RID", O.PrimaryKey, O.AutoInc, O.NotNull)
+  def RGname: Column[String] = column[String]("RG_NAME", O.NotNull)
+  def RGstartDate: Column[String] = column[String]("RG_STARTDATE", O.NotNull)
+  def RGdescription: Column[String] = column[String]("RG_DESCRIPTION")
+  def RGmasterRID: Column[Int] = column[Int]("RG_MASTERRID")
+  def RGavatar: Column[String] = column[String]("RG_AVATAR")
+  def RGendDate: Column[String] = column[String]("RG_ENDDATE")
+  def RGdeletedAt: Column[String] = column[String]("RG_DELETEDAT")
+
+  def * = (RGname, RGstartDate, RGdescription, RGmasterRID, RGavatar, RGendDate, RGdeletedAt, rid.?) <> (RadinGroup.tupled, RadinGroup.unapply)
 }
