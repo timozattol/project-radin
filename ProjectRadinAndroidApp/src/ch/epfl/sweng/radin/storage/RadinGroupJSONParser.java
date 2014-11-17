03c3dd61f8fdc4104fcf212a10a21ada8a6e273f
topali2 <thomas.batschelet@epfl.ch>
2014-11-17 15:42:43 +0100

    RadinGroupModelParserTest done

--- ProjectRadinAndroidApp/src/ch/epfl/sweng/radin/storage/RadinGroupJSONParser.java
+++ ProjectRadinAndroidApp/src/ch/epfl/sweng/radin/storage/RadinGroupJSONParser.java
@@ -6,7 +6,6 @@ package ch.epfl.sweng.radin.storage;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 import org.json.JSONArray;
@@ -14,20 +13,20 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
- * @author timozattol
- * A JSONParser for RadinGroupModel. A "fake" one, which always returns the same thing.
- * It's very ugly but it's just to have something that "works" for now.
+ * @author topali2
+ * A JSONParser for RadinGroupModel.
  */
 public class RadinGroupJSONParser implements JSONParser<RadinGroupModel> {
 
+	private DateTimeFormatter dtf = DateTimeFormat.forPattern("yyyy/MM/dd HH/mm");
 	/* (non-Javadoc)
 	 * @see ch.epfl.sweng.radin.storage.JSONParser#getModelsFromJson(java.util.List)
 	 */
 	@Override
 	public List<RadinGroupModel> getModelsFromJson(JSONObject jsonObject) throws JSONException {
 		List<RadinGroupModel> radinGroupModels = new ArrayList<RadinGroupModel>();
-		JSONArray jsonArray = jsonObject.getJSONArray("user");
-		DateTimeFormatter dtf = DateTimeFormat.forPattern("yyyy/mm/dd HH/mm");
+		JSONArray jsonArray = jsonObject.getJSONArray("radinGroup");
+		
 
 
 		for (int i = 0; i < jsonArray.length(); i++) {
@@ -35,45 +34,30 @@ public class RadinGroupJSONParser implements JSONParser<RadinGroupModel> {
 			JSONObject jsonData = jsonArray.getJSONObject(i);
 			RadinGroupModel radinGroupModel;
 			
-			if (jsonData.has("RG_MASTER_RID")) {
+			if (jsonData.has("RG_masterID")) {
 
 				radinGroupModel = new RadinGroupModel(
 						jsonData.getInt("RG_ID"),
-						dtf.parseDateTime(jsonData.getString("RG_CREATION_DATE")),
-						jsonData.getString("RG_NAME"),
-						jsonData.getString("RG_DESCRIPTION"),
-						jsonData.getString("RG_AVATAR"),
-						jsonData.getInt("RG_MASTER_RID"));
-
-				if (jsonData.has("RG_END_DATE")) {
-
-					radinGroupModel.setRadinGroupEndDateTime(dtf.parseDateTime(jsonData.getString("RG_END_DATE")));
-					radinGroupModel.setRadinGroupDeletionDateTime(
-							dtf.parseDateTime(jsonData.getString("RG_DELETED_AT")));
-				}
+						dtf.parseDateTime(jsonData.getString("RG_creationDate")),
+						jsonData.getString("RG_name"),
+						jsonData.getString("RG_description"),
+						jsonData.getString("RG_avatar"),
+						jsonData.getInt("RG_masterID"));
 
 			} else {
 
 				radinGroupModel = new RadinGroupModel(
 						jsonData.getInt("RG_ID"),
-						new DateTime(jsonData.getString("RG_CREATION_DATE")),
-						jsonData.getString("RG_NAME"),
-						jsonData.getString("RG_DESCRIPTION"),
-						jsonData.getString("RG_AVATAR"));
+						dtf.parseDateTime(jsonData.getString("RG_creationDate")),
+						jsonData.getString("RG_name"),
+						jsonData.getString("RG_description"),
+						jsonData.getString("RG_avatar"));
 
-				if (jsonData.has("RG_END_DATE")) {
-
-					radinGroupModel.setRadinGroupEndDateTime(dtf.parseDateTime(jsonData.getString("RG_END_DATE")));
-					radinGroupModel.setRadinGroupDeletionDateTime(
-							dtf.parseDateTime(jsonData.getString("RG_DELETED_AT")));
-				}
 			}
 			
 			radinGroupModels.add(radinGroupModel);
 		}
 
-		//        radinGroupModels.add(new RadinGroupModel(0, DateTime.now(), "Group 1", 
-		//                "A cool group", "img/avatar1.png"));
 		return radinGroupModels;
 	}
 
@@ -84,30 +68,20 @@ public class RadinGroupJSONParser implements JSONParser<RadinGroupModel> {
 	public JSONObject getJsonFromModels(List<RadinGroupModel> modelList) throws JSONException {
 		JSONObject json = new JSONObject();
 		JSONArray jsonArray = new JSONArray();
-		DateTimeFormatter dtf = DateTimeFormat.forPattern("yyyy/mm/dd HH/mm");
 
 		for (RadinGroupModel radinGroupModel : modelList) {
 
 			JSONObject jsonData = new JSONObject();
 
 			if (radinGroupModel.hasMasterID()) {
-				jsonData.put("RG_MASTER_RID", true);
+				jsonData.put("RG_masterID", radinGroupModel.getMasterID());
 			}
 
-			if (radinGroupModel.getRadinGroupEndDateTime() != null) {
-				jsonData.put("RG_END_DATE", radinGroupModel.getRadinGroupEndDateTime().toString(dtf));
-			}
-
-			if (radinGroupModel.getRadinGroupDeletionDateTime() != null) {
-				jsonData.put("RG_DELETED_AT", radinGroupModel.getRadinGroupDeletionDateTime().toString(dtf));
-			}
-
-			jsonData.put("_RID", radinGroupModel.getRadinGroupID());
-			jsonData.put("RG_NAME", radinGroupModel.getRadinGroupName());
-			jsonData.put("RG_CREATION_DATE", radinGroupModel.getGroupCreationDateTime().toString(dtf));
-			jsonData.put("RG_DESCRIPTION", radinGroupModel.getGroupDescription());
-			jsonData.put("RG_GROUP", "group radin"); // TODO what is group field?
-			jsonData.put("RG_AVATAR", radinGroupModel.getAvatar());
+			jsonData.put("RG_ID", radinGroupModel.getRadinGroupID());
+			jsonData.put("RG_name", radinGroupModel.getRadinGroupName());
+			jsonData.put("RG_creationDate", radinGroupModel.getGroupCreationDateTime().toString(dtf));
+			jsonData.put("RG_description", radinGroupModel.getGroupDescription());
+			jsonData.put("RG_avatar", radinGroupModel.getAvatar());
 
 			jsonArray.put(jsonData);
 		}
@@ -115,23 +89,6 @@ public class RadinGroupJSONParser implements JSONParser<RadinGroupModel> {
 		json.put("radinGroup", jsonArray);
 
 		return json;
-
-		//	try {
-		//		json.put("_RID", "0");
-		//		json.put("RG_NAME", "Group 1");
-		//		json.put("RG_CREATION_DATE", "2014/01/01/00/00");
-		//		json.put("RG_DESCRIPTION", "A cool group");
-		//		json.put("RG_GROUP", "group radin"); // TODO what is group field?
-		//		json.put("RG_MASTER_RID", "10");
-		//		json.put("RG_AVATAR", "img/avatar1.png");
-		//		json.put("RG_END_DATE", "2014/01/01/01/01");
-		//		json.put("RG_DELETED_AT", "2014/01/01/01/02");
-		//
-		//	} catch (JSONException e) {
-		//
-		//	}
-
-
 	}
 
 }
