b459a309a914a1e3b427c269e9da1a9c4220fe57
Simonchelbc <simon.lebail-collet@epfl.ch>
2014-11-18 05:06:30 +0100

    Working Database with insert and query methods using RadinGroupModel

--- ProjectRadinAndroidApp/src/ch/epfl/sweng/radin/databases/Database.java
+++ ProjectRadinAndroidApp/src/ch/epfl/sweng/radin/databases/Database.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import ch.epfl.sweng.radin.storage.Model;
 import ch.epfl.sweng.radin.storage.RadinGroupModel;
 import android.animation.AnimatorSet.Builder;
+import android.app.Application;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -15,35 +16,108 @@ import android.database.sqlite.SQLiteQueryBuilder;
 import android.os.Build;
 
 /**
- * @author Simonchelbc
- * 
+ * @author Simonchelbc Singleton Database object that can be used accross
+ *         multiple threads
  */
-public class Database {
-	private final DatabaseOpenHelper mOpenHelper;
+public final class Database {
+	private static Database dbInstance = null;
+	private static DatabaseOpenHelper mOpenHelper = null;
 
-	public Database(Context context) {
-		mOpenHelper = new DatabaseOpenHelper(context);
+	private static Context mCxt;
+
+	private Database() {
 	}
 
-	public Map<Long, Model> insert(List<RadinGroupModel> radinGroups) {
-		return mOpenHelper.insert(radinGroups);
+	/**
+	 * @param cxt
+	 *            {@link Context} of the Activity calling this method Shall
+	 *            always be called before using the Database
+	 */
+	public static synchronized void initialize(Context cxt) {
+		if (cxt == null) {
+			throw new IllegalArgumentException(Context.class.getName()
+					+ " cannot be null");
+		}
+		mCxt = cxt;
+		if (dbInstance == null) {
+			dbInstance = new Database();
+			mOpenHelper = new DatabaseOpenHelper(mCxt);
+		}
+	}
+
+	/**
+	 * used in every public method
+	 */
+	private static void instanceNotNullCheck() {
+		if (dbInstance == null || mOpenHelper == null) {
+			throw new IllegalStateException(Database.class.getName()
+					+ " is not initialized, " + "call initialize(..) first");
+		}
+	}
+
+	/**
+	 * @param radinGroups
+	 * @return a map that associated row id of 
+	 * the table where the model has been inserted
+	 */
+	public static Map<Long, Model> insert(List<RadinGroupModel> radinGroups) {
+		instanceNotNullCheck();
+		SQLiteDatabase sqlDb = mOpenHelper.getWritableDatabase();
+		if (radinGroups == null) {
+			throw new IllegalArgumentException(
+					"radinGroups pointing to null");
+		} else if (radinGroups.isEmpty()) {
+			throw new IllegalArgumentException(
+					"radinGroups cannot be Empty"); //TODO or do nothing instead?
+		} else {
+			final Map<Long, Model> rowIDToRadinGroup = new HashMap<Long, Model>(
+					radinGroups.size());
+			for (RadinGroupModel rg : radinGroups) {
+				long rowWhereInsertedID = insertSingleRadinGroupInDB(rg, sqlDb);
+				rowIDToRadinGroup.put(rowWhereInsertedID, rg);
+			}
+			return rowIDToRadinGroup;
+		}
 	}
 	
-	public Cursor query(String table, String[] columns,
-				String whereClause, String[] selectionArgs, String groupBy,
-				String having, String sortOrder) {
-		return mOpenHelper.query(table, columns, whereClause, selectionArgs, groupBy, having, sortOrder);
+	private static long insertSingleRadinGroupInDB(RadinGroupModel rg, SQLiteDatabase sqlDb) {
+		if (rg == null) {
+			throw new IllegalArgumentException(
+					"RadinGroup to insert cannot be null");
+		}
+		final long rowID = sqlDb.insert(
+				RadinGroupTableHelper.TABLE_RADIN_GROUP, null,
+				rg.getContentValues());
+		return rowID;
+	}
+
+	public static Cursor query(String table, String[] columns,
+			String whereClause, String[] selectionArgs, String groupBy,
+			String having, String sortOrder) {
+		instanceNotNullCheck();
+		SQLiteQueryBuilder buider = new SQLiteQueryBuilder();
+		buider.setTables(table);
+		Cursor cursor = buider.query(mOpenHelper.getReadableDatabase(), columns, whereClause,
+				selectionArgs, groupBy, having, sortOrder);
+
+		if (cursor == null) {
+			return null;
+		} else if (!cursor.moveToFirst()) {
+			cursor.close();
+			return null;
+		}
+		return cursor;
 	}
 
 	/**
 	 * @author Simonchelbc
 	 * 
 	 */
-	private static class DatabaseOpenHelper extends SQLiteOpenHelper {
+	private static final class DatabaseOpenHelper extends SQLiteOpenHelper {
 		public static final String DB_NAME = "radin_database";
-		public static final int VERSION = 1;
-		private final Context mHelperContext;
-		private SQLiteDatabase mDatabase;
+		private static final int VERSION = 1;
+		private static SQLiteDatabase mDatabase;
+
 		private TableHelper radinGroupTableHelper;
 		private TableHelper userRelationshipsTableHelper;
 
@@ -56,9 +130,8 @@ public class Database {
 		 *            Database version, 1 at first, used to see how to upgrade a
 		 *            database
 		 */
-		DatabaseOpenHelper(Context context) {
-			super(context, DB_NAME, null, VERSION);
-			mHelperContext = context;
+		private DatabaseOpenHelper(Context cxt) {
+			super(cxt, DB_NAME, null, VERSION);
 			radinGroupTableHelper = new RadinGroupTableHelper();
 			userRelationshipsTableHelper = new UserRelationshipsTableHelper();
 		}
@@ -67,68 +140,23 @@ public class Database {
 		public void onCreate(SQLiteDatabase db) {
 			mDatabase = db;
 			radinGroupTableHelper.onCreate(mDatabase);
-			userRelationshipsTableHelper.onCreate(mDatabase);
-		}
-
-		// TODO add to interface
-		public Map<Long, Model> insert(List<RadinGroupModel> radinGroups) {
-			if (radinGroups == null) {
-				throw new IllegalArgumentException(
-						"radinGroups pointing to null");
-			} else if (radinGroups.isEmpty()) {
-				throw new IllegalArgumentException(
-						"radinGroups cannot be Empty");
-			} else {
-				final Map<Long, Model> rowIDToRadinGroup = new HashMap<Long, Model>(
-						radinGroups.size());
-				for (RadinGroupModel rg : radinGroups) {
-					if (rg == null) {
-						throw new IllegalArgumentException(
-								"RadinGroup to insert cannot be null");
-					}
-					final long rowID = mDatabase.insert(
-							RadinGroupTableHelper.TABLE_RADIN_GROUP, null,
-							rg.getContentValues());
-					rowIDToRadinGroup.put(rowID, rg);
-				}
-				return rowIDToRadinGroup;
-			}
-		}
-
-		/**
-		 * @param table
-		 *            the name of the table to search into
-		 * @return
-		 */
-		public Cursor query(String table, String[] columns,
-				String whereClause, String[] selectionArgs, String groupBy,
-				String having, String sortOrder) {
-			SQLiteQueryBuilder buider = new SQLiteQueryBuilder();
-			buider.setTables(table);
-			Cursor cursor = buider.query(mDatabase, columns, whereClause,
-					selectionArgs, groupBy, having, sortOrder);
-
-			if (cursor == null) {
-				return null;
-			} else if (!cursor.moveToFirst()) {
-				cursor.close();
-				return null;
-			}
-			return cursor;
+			//userRelationshipsTableHelper.onCreate(mDatabase); //TODO uncomment and debug
+			//SQLiteException due to maybe not enough related table or 
+			//badly written statement
 		}
 
 		@Override
 		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 			switch (oldVersion) {
-			case 1:
-				// do something;
-				break;
-			case 2:
-				// do something
-				break;
-			default:
-				// default action
-				break;
+				case 1:
+					// do something;
+					break;
+				case 2:
+					// do something
+					break;
+				default:
+					// default action
+					break;
 			}
 		}
 	}
