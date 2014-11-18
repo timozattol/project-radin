6fb7b361bd445fbd08309af08846a7c9ef3df098
Ireneu Pla <ireneu.pla@epfl.ch>
2014-11-18 11:12:17 +0100

    connect to radingroups db, retrieve all and post

--- radin-server/app/models/Models.scala
+++
@@ -1,9 +0,0 @@
-package models
-
-class Models {
-
-  case class RadinGroup(RID: Int, RGname: String, RGstartDate: String,
-    RGdescription: String, RGgroup: String, RGmasterRID: Int,
-    RGavatar: String, RGendDate: String, RGdeletedAt: String);
-
-}
