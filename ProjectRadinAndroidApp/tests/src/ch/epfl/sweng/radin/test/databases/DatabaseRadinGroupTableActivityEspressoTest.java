db9d43a5c9faad7036c1cd9f96cf89b5ab513168
Simonchelbc <simon.lebail-collet@epfl.ch>
2014-11-18 05:07:29 +0100

    Add test on database sending RadinGroupModel and querying same database getting same result

--- ProjectRadinAndroidApp/tests/src/ch/epfl/sweng/radin/test/databases/DatabaseRadinGroupTableActivityEspressoTest.java
+++ ProjectRadinAndroidApp/tests/src/ch/epfl/sweng/radin/test/databases/DatabaseRadinGroupTableActivityEspressoTest.java
@@ -18,9 +18,10 @@ import android.util.Log;
  */
 public class DatabaseRadinGroupTableActivityEspressoTest extends
 		ActivityInstrumentationTestCase2<DatabaseRadinGroupTableActivity> {
-	private static final DateTime TODAY = DateTime.now();
-	private static final DateTime TOMORROW = TODAY.plusDays(1);
-	private static final DateTime AFTER_TOMORROW = TOMORROW.plusDays(1);
+	private static final DateTime TODAY_CLOCK = DateTime.now();
+	private static final String NOW = TODAY_CLOCK.toString();
+	private static final DateTime TOMORROW_CLOCK = TODAY_CLOCK.plusDays(1);
+	private static final DateTime AFTER_TOMORROW_CLOCK = TOMORROW_CLOCK.plusDays(1);
 	private static final int DEFAULT_RADIN_GROUP_ID = 0;
 	private static final int DEFAULT_MASTER_ID = 10;
 	private static final String DEFAULT_AVATAR = "<path-to-default-avatar>"; // TODO
@@ -31,8 +32,12 @@ public class DatabaseRadinGroupTableActivityEspressoTest extends
 	// avatars
 	private static final String DEFAULT_GROUP_NAME = "SuperRadins";
 	private static final String DEFAULT_DESCRIPTION = "Throw your debts away";
+	private static final String NEW_GROUP_NAME = "VeryRadins";
+	private static final String NEW_DESCRIPTION = "Throw the debters away";
+	
 	private static final String TAG = "DatabaseRadinGroupTableActivityEspressoTest";
 	// private DatabaseRadinGroupTableActivity mRadinGroupActivity;
+	private RadinGroupModel defaultRadinGroup;
 	
 	public DatabaseRadinGroupTableActivityEspressoTest() {
 		super(DatabaseRadinGroupTableActivity.class);
@@ -44,6 +49,9 @@ public class DatabaseRadinGroupTableActivityEspressoTest extends
 		super.setUp();
 		Log.d(TAG, "SETUP");
 		getActivity();
+		defaultRadinGroup = new RadinGroupModel(DEFAULT_RADIN_GROUP_ID, TODAY_CLOCK,
+				DEFAULT_GROUP_NAME, DEFAULT_DESCRIPTION, DEFAULT_AVATAR,
+				DEFAULT_MASTER_ID);
 		Log.d(TAG, "SETUP_DONE");
 	}
 
@@ -51,10 +59,7 @@ public class DatabaseRadinGroupTableActivityEspressoTest extends
 		assertNotNull(getActivity());
 	}
 	
-	public void testPuttingRadinGroupModelIntoDB() {
-		RadinGroupModel defaultRadinGroup = new RadinGroupModel(0, TODAY,
-				DEFAULT_GROUP_NAME, DEFAULT_DESCRIPTION, DEFAULT_AVATAR,
-				DEFAULT_MASTER_ID);
+	public void testInsertOneRadinGroupInDB() {
 		List<RadinGroupModel> radinGroups = new ArrayList<RadinGroupModel>();
 		radinGroups.add(defaultRadinGroup);
 		getActivity().sendRadinGroupModelToDB(radinGroups);
@@ -62,8 +67,40 @@ public class DatabaseRadinGroupTableActivityEspressoTest extends
 				.getEverythingFromRadinGroupTable();
 		assertEquals(1, rowsColumnToValue.size());
 		Map<String, String> defaultRadinGroupAsMap = rowsColumnToValue.get(0);
-		assertEquals(0,
-				defaultRadinGroupAsMap.get(RadinGroupTableHelper.Column.RID
-						.getSqlName()));
+		assertEquals(DEFAULT_RADIN_GROUP_ID,
+				Integer.parseInt(defaultRadinGroupAsMap.get(RadinGroupTableHelper.Column.RID
+						.toString())));
+		assertEquals(DEFAULT_AVATAR, 
+				defaultRadinGroupAsMap.get(RadinGroupTableHelper
+						.Column.RG_AVATAR.toString()));
+		assertEquals(DEFAULT_DESCRIPTION, 
+				defaultRadinGroupAsMap.get(RadinGroupTableHelper
+						.Column.RG_DESCRIPTION.toString()));
+		assertEquals(DEFAULT_GROUP_NAME, 
+				defaultRadinGroupAsMap.get(RadinGroupTableHelper
+						.Column.RG_NAME.toString()));
+		assertEquals(DEFAULT_MASTER_ID, 
+				Integer.parseInt(defaultRadinGroupAsMap.
+						get(RadinGroupTableHelper.Column
+								.RG_MASTER_RID.toString())));
+		assertNull(defaultRadinGroupAsMap.get(
+				RadinGroupTableHelper.Column.RG_END_DATE.toString()));
+		assertNull(defaultRadinGroupAsMap.get(
+				RadinGroupTableHelper.Column.RG_DELETED_AT.toString()));
+		
+		
+//		assertEquals(TODAY_CLOCK.toString(), defaultRadinGroupAsMap.get(RadinGroupTableHelper.
+//				Column.RG_CREATION_DATE.toString())); //hard to test on time because 
+		//toString captures current time
+	}
+	
+	public void testInsertEmptyListOfRadinGroups() throws Exception {
+		try {
+			List<RadinGroupModel> radinGroups = new ArrayList<RadinGroupModel>();		
+			getActivity().sendRadinGroupModelToDB(radinGroups);
+			fail("radinGroups cannot be Empty");
+		} catch (IllegalArgumentException e) {
+			//good
+		}
 	}
 }
