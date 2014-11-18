ab1c03d7f3677f4fce2f5609da81f630cfad3301
Simonchelbc <simon.lebail-collet@epfl.ch>
2014-11-18 05:03:13 +0100

    Refactor look to make it more readable

--- ProjectRadinAndroidApp/src/ch/epfl/sweng/radin/databases/UserRelationshipsTableHelper.java
+++ ProjectRadinAndroidApp/src/ch/epfl/sweng/radin/databases/UserRelationshipsTableHelper.java
@@ -7,13 +7,16 @@ import android.database.sqlite.SQLiteDatabase;
  *
  */
 public class UserRelationshipsTableHelper implements TableHelper {
-	private static final String CREATE_TABLE_USER_RELATIONSHIP = "CREATE TABLE USER_RELATIONSHIP" 
-			+"(_UID_SOURCE INT NOT NULL,"
+	private static final String CREATE_TABLE_USER_RELATIONSHIP = "CREATE TABLE USER_RELATIONSHIP(" 
+			+ "_UID_SOURCE INT NOT NULL,"
 			+ "_UID_TARGET INT NOT NULL,"
-			+ "RELATION INT NOT NULL,"
-			+ "FOREIGN KEY (_UID_SOURCE, _UID_TARGET), REFERENCES USER(_UID, _UID),"
-			+ "PRIMARY KEY (_UID_SOURCE, _UID_TARGET));";
+			+ "UR_RELATION INT NOT NULL,"
+			+ "FOREIGN KEY (_UID_SOURCE, _UID_TARGET),"
+			+ "REFERENCES USER(_UID, _UID),"
+			+ "PRIMARY KEY (_UID_SOURCE, _UID_TARGET)" 
+			+ ");";
 
+	UserRelationshipsTableHelper() { }
 	@Override
 	public void onCreate(SQLiteDatabase db) {
 		db.execSQL(CREATE_TABLE_USER_RELATIONSHIP);
