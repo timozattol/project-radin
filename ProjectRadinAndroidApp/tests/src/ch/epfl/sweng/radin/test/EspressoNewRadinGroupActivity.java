eca0454213e17b990154613262ee7a00a3e163c9
Joël Kaufmann <joel.kaufmann@epfl.ch>
2014-11-19 09:12:16 +0100

    update tests to work on emulator & device

--- ProjectRadinAndroidApp/tests/src/ch/epfl/sweng/radin/test/EspressoNewRadinGroupActivity.java
+++ ProjectRadinAndroidApp/tests/src/ch/epfl/sweng/radin/test/EspressoNewRadinGroupActivity.java
@@ -12,7 +12,7 @@ import com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers;
 
 /**
  * A class for testing everything that concerns RadinGroups
- * @author CedricCook
+ * @author Jokau
  *
  */
 public class EspressoNewRadinGroupActivity extends ActivityInstrumentationTestCase2<NewRadinGroupActivity> {
@@ -29,6 +29,9 @@ public class EspressoNewRadinGroupActivity extends ActivityInstrumentationTestCa
 		getActivity();
 	}
 	
+	private void scrollTo(int rId) {
+		Espresso.onView(ViewMatchers.withId(rId)).perform(ViewActions.scrollTo());
+	}
 	
 	public void testNewRadinGroup() {	
 		//TODO add restriction on special characters
@@ -38,19 +41,24 @@ public class EspressoNewRadinGroupActivity extends ActivityInstrumentationTestCa
 		Espresso.onView(ViewMatchers.withId(R.id.people)).perform(ViewActions.click());
 		Espresso.onView(ViewMatchers.withText("julie")).perform(ViewActions.click());
 		Espresso.onView(ViewMatchers.withText("OK")).perform(ViewActions.click());
+		scrollTo(R.id.create);
 		Espresso.onView(ViewMatchers.withId(R.id.create)).perform(ViewActions.click());
 		//should close activity and toast list created
 	}
 	
 	public void testNewRadinGroupWithoutName() {
+		scrollTo(R.id.people);
 		Espresso.onView(ViewMatchers.withId(R.id.people)).perform(ViewActions.click());
 		Espresso.onView(ViewMatchers.withText("julie")).perform(ViewActions.click());
 		Espresso.onView(ViewMatchers.withText("OK")).perform(ViewActions.click());
+		scrollTo(R.id.create);
 		Espresso.onView(ViewMatchers.withId(R.id.create)).perform(ViewActions.click());
 		// should toast no name and stay on activity
 	}
 	public void testNewRadinGroupWithoutParticipant() {
+		scrollTo(R.id.edit_name);
 		Espresso.onView(ViewMatchers.withId(R.id.edit_name)).perform(ViewActions.typeText("Ma super liste!"));
+		scrollTo(R.id.create);
 		Espresso.onView(ViewMatchers.withId(R.id.create)).perform(ViewActions.click());
 		// should toast no people on list and stay on activity
 	}
--- ProjectRadinAndroidApp/tests/src/ch/epfl/sweng/radin/test/EspressoRadinGroupAddExpenseActivity.java
+++ ProjectRadinAndroidApp/tests/src/ch/epfl/sweng/radin/test/EspressoRadinGroupAddExpenseActivity.java
@@ -8,18 +8,12 @@ import ch.epfl.sweng.radin.R;
 import ch.epfl.sweng.radin.RadinGroupAddExpenseActivity;
 
 import com.google.android.apps.common.testing.ui.espresso.Espresso;
-import com.google.android.apps.common.testing.ui.espresso.ViewAction;
-import com.google.android.apps.common.testing.ui.espresso.action.GeneralLocation;
-import com.google.android.apps.common.testing.ui.espresso.action.GeneralSwipeAction;
-import com.google.android.apps.common.testing.ui.espresso.action.Press;
-import com.google.android.apps.common.testing.ui.espresso.action.Swipe;
 import com.google.android.apps.common.testing.ui.espresso.action.ViewActions;
 import com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions;
 import com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers;
 
 /**
  * GUI tests on RadinGroupAddExpenseActivity
- * CAUTION, only works with emulator (soft keyboard hides items)
  * 
  * @author Jolauka
  */
@@ -29,14 +23,14 @@ public class EspressoRadinGroupAddExpenseActivity extends
 		super(RadinGroupAddExpenseActivity.class);
 	}
 	
-	/**
-	 * http://qathread.blogspot.de/2014/01/discovering-espresso-for-android-swiping.html
-	 * swipes up
-	 */
-	private ViewAction swipeUp() {
-	    return new GeneralSwipeAction(Swipe.FAST, GeneralLocation.CENTER,
-	    		GeneralLocation.TOP_CENTER, Press.FINGER);
-	}
+//	/**
+//	 * http://qathread.blogspot.de/2014/01/discovering-espresso-for-android-swiping.html
+//	 * swipes up
+//	 */
+//	private ViewAction swipeUp() {
+//	    return new GeneralSwipeAction(Swipe.FAST, GeneralLocation.CENTER,
+//	    		GeneralLocation.TOP_CENTER, Press.FINGER);
+//	}
 	
 	@Override
 	public void setUp() throws Exception {
@@ -47,21 +41,30 @@ public class EspressoRadinGroupAddExpenseActivity extends
 		getActivity();
 	}
 	
+	private void scrollTo(int rId) {
+		Espresso.onView(ViewMatchers.withId(rId)).perform(ViewActions.scrollTo());
+	}
+	
+	
+	
 	public void testAddCreditor() {	
 		//purpose TextField
 		Espresso.onView(ViewMatchers.withId(R.id.purpose_title)).perform(ViewActions.typeText("Commissions du 15.11.14"));
 		
-		//open creditor dialog button		
+		//open creditor dialog button
+		scrollTo(R.id.add_creditor);
 		Espresso.onView(ViewMatchers.withId(R.id.add_creditor)).perform(ViewActions.click());
 		//Dialog : select creditor
 		Espresso.onView(ViewMatchers.withText("julie")).perform(ViewActions.click());
 		Espresso.onView(ViewMatchers.withText("OK")).perform(ViewActions.click());
 		
+		scrollTo(R.id.creditor_selected);
 		Espresso.onView(ViewMatchers.withId(R.id.creditor_selected)).check(ViewAssertions.matches(ViewMatchers.withText("julie")));
 	}
 	
 	public void testAddDebtor() {
 		//open debtor dialog button
+		scrollTo(R.id.add_debtors);
 		Espresso.onView(ViewMatchers.withId(R.id.add_debtors)).perform(ViewActions.click());
 		
 		//Dialog : select debtors
@@ -69,21 +72,25 @@ public class EspressoRadinGroupAddExpenseActivity extends
 		Espresso.onView(ViewMatchers.withText("Igor")).perform(ViewActions.click());
 		Espresso.onView(ViewMatchers.withText("JT")).perform(ViewActions.click());
 		Espresso.onView(ViewMatchers.withText("OK")).perform(ViewActions.click());
-				
+		
+		scrollTo(R.id.debtors_selected);
 		Espresso.onView(ViewMatchers.withId(R.id.debtors_selected)).check(ViewAssertions.matches(ViewMatchers.withText(" julie Igor JT")));
 	}
 	
 	public void testAmountField() {
 		//should allow only numericals
+		scrollTo(R.id.amount_Field);
 		Espresso.onView(ViewMatchers.withId(R.id.amount_Field)).perform(ViewActions.typeText("Salut")).check(ViewAssertions.matches(ViewMatchers.withText("")));
 		Espresso.onView(ViewMatchers.withId(R.id.amount_Field)).perform(ViewActions.typeText("234")).check(ViewAssertions.matches(ViewMatchers.withText("234")));
 	}
 	
 	public void testNoCreditor() {
 		//purpose TextField
+		scrollTo(R.id.purpose_title);
 		Espresso.onView(ViewMatchers.withId(R.id.purpose_title)).perform(ViewActions.typeText("Commissions du 15.11.14"));
 		
-		//open debtor dialog button
+		//open debtor dialog button.
+		scrollTo(R.id.add_debtors);
 		Espresso.onView(ViewMatchers.withId(R.id.add_debtors)).perform(ViewActions.click());
 		//Dialog : select debtors
 		Espresso.onView(ViewMatchers.withText("julie")).perform(ViewActions.click());
@@ -91,10 +98,11 @@ public class EspressoRadinGroupAddExpenseActivity extends
 		Espresso.onView(ViewMatchers.withText("JT")).perform(ViewActions.click());
 		Espresso.onView(ViewMatchers.withText("OK")).perform(ViewActions.click());
 			
-		Espresso.onView(ViewMatchers.withId(R.id.addExpenseRadinGroupLayout)).perform(swipeUp());
-		
+		//Espresso.onView(ViewMatchers.withId(R.id.addExpenseRadinGroupLayout)).perform(swipeUp());
+		scrollTo(R.id.amount_Field);
 		Espresso.onView(ViewMatchers.withId(R.id.amount_Field)).perform(ViewActions.typeText("234"));
 		
+		scrollTo(R.id.add_expense_button);
 		Espresso.onView(ViewMatchers.withId(R.id.add_expense_button)).perform(ViewActions.click());
 		//should be fine (because of default creditor)
 		Log.i("ACTIVITY", getActivity().toString());
@@ -102,24 +110,29 @@ public class EspressoRadinGroupAddExpenseActivity extends
 	
 	public void testNoDebtors() {
 		//purpose TextField
+		scrollTo(R.id.purpose_title);
 		Espresso.onView(ViewMatchers.withId(R.id.purpose_title)).perform(ViewActions.typeText("Commissions du 15.11.14"));
 				
-		//open creditor dialog button		
+		//open creditor dialog button
+		scrollTo(R.id.add_creditor);
 		Espresso.onView(ViewMatchers.withId(R.id.add_creditor)).perform(ViewActions.click());
 		//Dialog : select creditor
 		Espresso.onView(ViewMatchers.withText("julie")).perform(ViewActions.click());
 		Espresso.onView(ViewMatchers.withText("OK")).perform(ViewActions.click());
 		
-		Espresso.onView(ViewMatchers.withId(R.id.addExpenseRadinGroupLayout)).perform(swipeUp());
+		//Espresso.onView(ViewMatchers.withId(R.id.addExpenseRadinGroupLayout)).perform(swipeUp());
 		
+		scrollTo(R.id.amount_Field);
 		Espresso.onView(ViewMatchers.withId(R.id.amount_Field)).perform(ViewActions.typeText("234")).check(ViewAssertions.matches(ViewMatchers.withText("234")));
 		
+		scrollTo(R.id.add_expense_button);
 		Espresso.onView(ViewMatchers.withId(R.id.add_expense_button)).perform(ViewActions.click());
 		//should toast a message, stay on RadinGroupAddExpenseActivity activity
 	}
 	
 	public void testNoName() {
-		//open creditor dialog button		
+		//open creditor dialog button	
+		scrollTo(R.id.add_creditor);
 		Espresso.onView(ViewMatchers.withId(R.id.add_creditor)).perform(ViewActions.click());
 			
 		//Dialog : select creditor
@@ -127,6 +140,7 @@ public class EspressoRadinGroupAddExpenseActivity extends
 		Espresso.onView(ViewMatchers.withText("OK")).perform(ViewActions.click());
 		
 		//open debtor dialog button
+		scrollTo(R.id.add_debtors);
 		Espresso.onView(ViewMatchers.withId(R.id.add_debtors)).perform(ViewActions.click());
 		//Dialog : select debtors
 		Espresso.onView(ViewMatchers.withText("julie")).perform(ViewActions.click());
@@ -134,10 +148,12 @@ public class EspressoRadinGroupAddExpenseActivity extends
 		Espresso.onView(ViewMatchers.withText("JT")).perform(ViewActions.click());
 		Espresso.onView(ViewMatchers.withText("OK")).perform(ViewActions.click());
 		
-		Espresso.onView(ViewMatchers.withId(R.id.addExpenseRadinGroupLayout)).perform(swipeUp());
+		//Espresso.onView(ViewMatchers.withId(R.id.addExpenseRadinGroupLayout)).perform(swipeUp());
 		
+		scrollTo(R.id.amount_Field);
 		Espresso.onView(ViewMatchers.withId(R.id.amount_Field)).perform(ViewActions.typeText("234"));
 		
+		scrollTo(R.id.add_expense_button);
 		Espresso.onView(ViewMatchers.withId(R.id.add_expense_button)).perform(ViewActions.click());
 		//should toast a message, stay on RadinGroupAddExpenseActivity activity
 	}
