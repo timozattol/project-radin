6fb887517f3d5c350e19d1e6a1b06849e6a8aa56
Timothée Lottaz <timozattol@gmail.com>
2014-11-29 15:06:52 +0100

    Add a method to get all transactions by radingroup id

--- ProjectRadinAndroidApp/src/ch/epfl/sweng/radin/storage/managers/TransactionStorageManager.java
+++ ProjectRadinAndroidApp/src/ch/epfl/sweng/radin/storage/managers/TransactionStorageManager.java
@@ -3,6 +3,8 @@
  */
 package ch.epfl.sweng.radin.storage.managers;
 
+import ch.epfl.sweng.radin.callback.RadinListener;
+import ch.epfl.sweng.radin.storage.RequestType;
 import ch.epfl.sweng.radin.storage.TransactionModel;
 import ch.epfl.sweng.radin.storage.parsers.JSONParser;
 import ch.epfl.sweng.radin.storage.parsers.TransactionJSONParser;
@@ -42,5 +44,17 @@ public final class TransactionStorageManager extends StorageManager<TransactionM
     @Override
     protected String getTypeUrl() {
         return "transactions";
+    }
+    
+    public void getAllByRadinGroupId(int radinGroupId, RadinListener<TransactionModel> callback) {
+        if (isConnected()) {
+            if (!isHashMatchServer()) {
+                ServerConnectionTask connTask = new ServerConnectionTask(callback, RequestType.GET,
+                        SERVER_BASE_URL + "transactions/" + radinGroupId);
+                connTask.execute();
+                return;
+            }
+        }
+        //TODO take the data from the local DB
     }
 }
