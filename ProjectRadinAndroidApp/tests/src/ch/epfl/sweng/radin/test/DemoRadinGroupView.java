ece60a7d17129524adb33b7e7b27bc33ccdcaeb4
Joël Kaufmann <joel.kaufmann@epfl.ch>
2014-12-01 10:02:00 +0100

    push to work on my laptop Demo updated, lib problem

--- ProjectRadinAndroidApp/tests/src/ch/epfl/sweng/radin/test/DemoRadinGroupView.java
+++ ProjectRadinAndroidApp/tests/src/ch/epfl/sweng/radin/test/DemoRadinGroupView.java
@@ -1,30 +1,30 @@
 package ch.epfl.sweng.radin.test;
 
-import org.joda.time.DateTime;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.util.ArrayList;
+import java.util.List;
 
-import ch.epfl.sweng.radin.ActionBar;
-import ch.epfl.sweng.radin.storage.RadinGroupModel;
-import android.content.Intent;
-import android.test.AndroidTestCase;
+import javax.annotation.meta.When;
 
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
+import org.json.JSONObject;
+//import org.mockito.Mockito;
 
-
-import org.joda.time.DateTime;
-import ch.epfl.sweng.radin.storage.parsers.RadinGroupJSONParser;
-import android.content.Intent;
+import android.os.storage.StorageManager;
 import android.test.ActivityInstrumentationTestCase2;
-import ch.epfl.sweng.radin.ActionBar;
 import ch.epfl.sweng.radin.LoginActivity;
-import ch.epfl.sweng.radin.NewRadinGroupActivity;
 import ch.epfl.sweng.radin.R;
+import ch.epfl.sweng.radin.callback.RadinListener;
+import ch.epfl.sweng.radin.callback.StorageManagerRequestStatus;
 import ch.epfl.sweng.radin.storage.RadinGroupModel;
+import ch.epfl.sweng.radin.storage.RequestType;
+import ch.epfl.sweng.radin.storage.managers.*;
+import ch.epfl.sweng.radin.storage.parsers.RadinGroupJSONParser;
 
 import com.google.android.apps.common.testing.ui.espresso.Espresso;
+import com.google.android.apps.common.testing.ui.espresso.action.Tapper.Status;
 import com.google.android.apps.common.testing.ui.espresso.action.ViewActions;
-import com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions;
 import com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers;
 
 /**
@@ -33,7 +33,23 @@ import com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers;
  *
  */
 public class DemoRadinGroupView extends ActivityInstrumentationTestCase2<LoginActivity> {
-	
+	private HttpURLConnection connection;
+	private static final String JSON_RESPONSE = "{\"radinGroup\":[" +
+			"{\"RG_name\":\"radinGroup\"," +
+			"\"RG_creationDate\":\"2014/11/28 10/11\"," +
+			"\"RG_description\":\"description bidon\"," +
+			"\"RG_masterID\":1," +
+			"\"RG_avatar\":\"\"," +
+			"\"RG_deletedAt\":\"\"," +
+			"\"RG_ID\":1}," +
+			"{\"RG_name\":\"radinGroup2\"," +
+			"\"RG_creationDate\":\"2014/11/29 10/11\"," +
+			"\"RG_description\":\"description balo\"," +
+			"\"RG_masterID\":2," +
+			"\"RG_avatar\":\"\"," +
+			"\"RG_deletedAt\":\"\"," +
+			"\"RG_ID\":2}]}";
+
 	public DemoRadinGroupView() {
 		super(LoginActivity.class);
 	}
@@ -41,66 +57,26 @@ public class DemoRadinGroupView extends ActivityInstrumentationTestCase2<LoginAc
 	@Override
 	public void setUp() throws Exception {
 		super.setUp();
-		
-		RadinGroupModel rgModel = new RadinGroupModel(
-                0, DateTime.now(), "My example list", "A simple example list", 
-                "https://www.youtube.com/watch?v=dQw4w9WgXcQ");
-		
-		Intent myIntent = new Intent();
-		myIntent.putExtras(ActionBar.makeModelToBundle(rgModel));
-		setActivityIntent(myIntent);
-		getActivity();
+//		connection = Mockito.mock(HttpURLConnection.class);
+		 InputStream dataStream = new ByteArrayInputStream(JSON_RESPONSE.getBytes());
+//		 Mockito.doReturn(200).when(connection).getResponseCode();
+//		 Mockito.doReturn(dataStream).when(connection).getInputStream();
+//		 
+		 getActivity();
 	}
+	
 	
 	private void scrollTo(int rId) {
 		Espresso.onView(ViewMatchers.withId(rId)).perform(ViewActions.scrollTo());
 	}
 	
-	public void test1()  {
-		RadinGroupJSONParser JSONParser = Mockito.mock(RadinGroupJSONParser.class);
-		  // define return value for method getUniqueId()
-		  JSONParser.when(JSONParser.getUniqueId()).thenReturn(43);
-		  
-		  // TODO use mock in test.... 
-		  
-		  // now check if method testing was called with the parameter 12 
-		  Mockito.verify(test).testing(Matchers.eq(12));
-		  
-		  // was the method called twice?
-		  Mockito.verify(test, Mockito.times(2));
-		  
-	
-	
-	
-	
-	public void testNewRadinGroup() {	
-		//TODO add restriction on special characters
-		Espresso.onView(ViewMatchers.withId(R.id.edit_name)).perform(ViewActions.typeText("Ma super liste!"))
-		.check(ViewAssertions.matches(ViewMatchers.withText("Ma super liste!")));
-		
-		Espresso.onView(ViewMatchers.withId(R.id.people)).perform(ViewActions.click());
-		Espresso.onView(ViewMatchers.withText("julie")).perform(ViewActions.click());
-		Espresso.onView(ViewMatchers.withText("OK")).perform(ViewActions.click());
-		scrollTo(R.id.create);
-		Espresso.onView(ViewMatchers.withId(R.id.create)).perform(ViewActions.click());
-		//should close activity and toast list created
+	public void testAll() {
+		//go to RadinGroupView
+		Espresso.onView(ViewMatchers.withId(R.id.login)).perform(ViewActions.typeText("topali2"));
+		Espresso.onView(ViewMatchers.withId(R.id.password)).perform(ViewActions.typeText("to2Ra1din@pali"));
+		Espresso.onView(ViewMatchers.withId(R.id.loginButton)).perform(ViewActions.click());
+
+		Espresso.onView(ViewMatchers.withId(R.id.myRadinGroupBtn)).perform(ViewActions.click());
+//		Espresso.onView(viewMatcher)
 	}
-	
-	public void testNewRadinGroupWithoutName() {
-		scrollTo(R.id.people);
-		Espresso.onView(ViewMatchers.withId(R.id.people)).perform(ViewActions.click());
-		Espresso.onView(ViewMatchers.withText("julie")).perform(ViewActions.click());
-		Espresso.onView(ViewMatchers.withText("OK")).perform(ViewActions.click());
-		scrollTo(R.id.create);
-		Espresso.onView(ViewMatchers.withId(R.id.create)).perform(ViewActions.click());
-		// should toast no name and stay on activity
-	}
-	public void testNewRadinGroupWithoutParticipant() {
-		scrollTo(R.id.edit_name);
-		Espresso.onView(ViewMatchers.withId(R.id.edit_name)).perform(ViewActions.typeText("Ma super liste!"));
-		scrollTo(R.id.create);
-		Espresso.onView(ViewMatchers.withId(R.id.create)).perform(ViewActions.click());
-		// should toast no people on list and stay on activity
-	}
-	
 }
