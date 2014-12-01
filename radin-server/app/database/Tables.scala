9037a42f45738cdb9bba5199ac641763f5d9ac6f
Simonchelbc <simon.lebail-collet@epfl.ch>
2014-12-01 10:32:54 +0100

    UserRealationships now extends UserRelationship

--- radin-server/app/database/Tables.scala
+++ radin-server/app/database/Tables.scala
@@ -8,7 +8,7 @@ import play.api.mvc.BodyParsers._
 import play.api.libs.json.Json
 import play.api.libs.json.Json._
 
-class Tables {
+object Tables {
 
   lazy val radinGroups = TableQuery[RadinGroups]
 
@@ -23,7 +23,6 @@ class Tables {
     def Tcurrency: Column[Int] = column[Int]("T_CURRENCY", O.NotNull)
     def Tdate: Column[String] = column[String]("T_DATE", O.NotNull)
     def Ttype: Column[Int] = column[Int]("T_TRANSACTIONTYPE", O.NotNull)
-    def Tjustificative: Column[String] = column[String]("T_JUSTIFICATIVE")
     def TmasterTID: Column[Int] = column[Int]("T_MASTERTID")
     def TdeletedAt: Column[String] = column[String]("T_DELETEDAT")
 
@@ -33,22 +32,7 @@ class Tables {
 
   lazy val users = TableQuery[Users]
 
-  case class UserRelationship(uidSource: Int, uidTarget: Int, URrelation: Int)
-  
-  class UserRelationships(tag: Tag) extends Table[(Int, Int, Int)](tag, "USER_RELATIONSHIP") {
-
-    def uidSource: Column[Int] = column[Int]("UID_SOURCE", O.NotNull)
-    def uidTarget: Column[Int] = column[Int]("UID_TARGET", O.NotNull)
-    def URrelation: Column[Int] = column[Int]("UR_RELATION", O.NotNull)
-
-    def sourceUser = foreignKey("SOURCE_USER", uidSource, users)(_.U_ID)
-    def targetUser = foreignKey("TARGET_USER", uidTarget, users)(_.U_ID)
-    def URpk = primaryKey("URpk", (uidSource, uidTarget))
-
-    def * = (uidSource, uidTarget, URrelation) <> (UserRelationship.tupled, UserRelationship.unapply)
-  }
-  
-  lazy val userRelationships = TableQuery[UserRelationship]
+	lazy val userRelationships = TableQuery[UserRelationships]
 
   class MemberInRadin(tag: Tag) extends Table[(Int, Int, String, Int, String)](tag, "MEMBER_IN_RADIN") {
 
@@ -125,37 +109,51 @@ class Tables {
   }
   lazy val userConcernedByTransactions = TableQuery[UserConcernedByTransaction]
 
-}
+  case class RadinGroup(RG_name: String, RG_creationDate: String, RG_description: String, RG_masterID: Int, RG_avatar: String, RG_deletedAt: String, RG_ID: Option[Int] = None)
 
-case class RadinGroup(RG_name: String, RG_creationDate: String, RG_description: String, RG_masterID: Int, RG_avatar: String, RG_deletedAt: String, RG_ID: Option[Int] = None)
-
-class RadinGroups(tag: Tag) extends Table[RadinGroup](tag, "RADINGROUP") {
-  def rid: Column[Int] = column[Int]("RID", O.PrimaryKey, O.AutoInc)
-  def RGname: Column[String] = column[String]("RG_NAME", O.NotNull)
-  def RGstartDate: Column[String] = column[String]("RG_STARTDATE", O.NotNull)
-  def RGdescription: Column[String] = column[String]("RG_DESCRIPTION")
-  def RGmasterRID: Column[Int] = column[Int]("RG_MASTERRID")
-  def RGavatar: Column[String] = column[String]("RG_AVATAR")
-  def RGdeletedAt: Column[String] = column[String]("RG_DELETEDAT")
-
-  def * = (RGname, RGstartDate, RGdescription, RGmasterRID, RGavatar, RGdeletedAt, rid.?) <> (RadinGroup.tupled, RadinGroup.unapply)
-}
-
-case class User(U_firstName: String, U_lastName: String, U_username: String, U_password: String, U_email: String, U_address: String, U_iban: String, U_bicSwift: String, U_avatar: String, U_deletedAt: String, U_ID: Option[Int] = None)
-
-class Users(tag: Tag) extends Table[User](tag, "USER") {
-
-  def U_firstName: Column[String] = column[String]("U_NAME", O.NotNull)
-  def U_lastName: Column[String] = column[String]("U_LAST", O.NotNull)
-  def U_username: Column[String] = column[String]("U_USERNAME", O.NotNull)
-  def U_password: Column[String] = column[String]("U_PASSWORD", O.NotNull)
-  def U_email: Column[String] = column[String]("U_IBAN")
-  def U_address: Column[String] = column[String]("U_LANGUAGE", O.NotNull)
-  def U_iban: Column[String] = column[String]("U_ADDRESS")
-  def U_bicSwift: Column[String] = column[String]("U_OPTIONS", O.NotNull)
-  def U_avatar: Column[String] = column[String]("U_AVATAR")
-  def U_deletedAt: Column[String] = column[String]("U_DELETED_AT")
-  def U_ID: Column[Int] = column[Int]("UID", O.PrimaryKey, O.AutoInc, O.NotNull)
-
-  def * = (U_firstName, U_lastName, U_username, U_password, U_email, U_address, U_iban, U_bicSwift, U_avatar, U_deletedAt, U_ID.?) <> (User.tupled, User.unapply)
+  class RadinGroups(tag: Tag) extends Table[RadinGroup](tag, "RADINGROUP") {
+	  def rid: Column[Int] = column[Int]("RID", O.PrimaryKey, O.AutoInc)
+	  def RGname: Column[String] = column[String]("RG_NAME", O.NotNull)
+	  def RGstartDate: Column[String] = column[String]("RG_STARTDATE", O.NotNull)
+	  def RGdescription: Column[String] = column[String]("RG_DESCRIPTION")
+	  def RGmasterRID: Column[Int] = column[Int]("RG_MASTERRID")
+	  def RGavatar: Column[String] = column[String]("RG_AVATAR")
+	  def RGdeletedAt: Column[String] = column[String]("RG_DELETEDAT")
+	  
+	  def * = (RGname, RGstartDate, RGdescription, RGmasterRID, RGavatar, RGdeletedAt, rid.?) <> (RadinGroup.tupled, RadinGroup.unapply)
+  }
+  
+  case class UserRelationship(source: Int, target: Int, URrelation: Int)
+  
+  class UserRelationships(tag: Tag) extends Table[UserRelationship](tag, "USER_RELATIONSHIP") {
+  	
+  	def uidSource: Column[Int] = column[Int]("UID_SOURCE", O.NotNull)
+		def uidTarget: Column[Int] = column[Int]("UID_TARGET", O.NotNull)
+		def URrelation: Column[Int] = column[Int]("UR_RELATION", O.NotNull)
+		
+		def sourceUser = foreignKey("SOURCE_USER", uidSource, users)(_.U_ID)
+		def targetUser = foreignKey("TARGET_USER", uidTarget, users)(_.U_ID)
+		def URpk = primaryKey("URpk", (uidSource, uidTarget))
+		
+		def * = (uidSource, uidTarget, URrelation) <> (UserRelationship.tupled, UserRelationship.unapply)
+  }
+  
+  		
+  case class User(U_firstName: String, U_lastName: String, U_username: String, U_password: String, U_email: String, U_address: String, U_iban: String, U_bicSwift: String, U_avatar: String, U_deletedAt: String, U_ID: Option[Int] = None)
+  
+  class Users(tag: Tag) extends Table[User](tag, "USER") {
+  	def U_firstName: Column[String] = column[String]("U_NAME", O.NotNull)
+		def U_lastName: Column[String] = column[String]("U_LAST", O.NotNull)
+		def U_username: Column[String] = column[String]("U_USERNAME", O.NotNull)
+		def U_password: Column[String] = column[String]("U_PASSWORD", O.NotNull)
+		def U_email: Column[String] = column[String]("U_IBAN")
+		def U_address: Column[String] = column[String]("U_LANGUAGE", O.NotNull)
+		def U_iban: Column[String] = column[String]("U_ADDRESS")
+		def U_bicSwift: Column[String] = column[String]("U_OPTIONS", O.NotNull)
+		def U_avatar: Column[String] = column[String]("U_AVATAR")
+		def U_deletedAt: Column[String] = column[String]("U_DELETED_AT")
+		def U_ID: Column[Int] = column[Int]("UID", O.PrimaryKey, O.AutoInc, O.NotNull)
+		
+		def * = (U_firstName, U_lastName, U_username, U_password, U_email, U_address, U_iban, U_bicSwift, U_avatar, U_deletedAt, U_ID.?) <> (User.tupled, User.unapply)
+  }
 }
