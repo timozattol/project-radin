e74897ed14b8e78a96466ad99c4d2dee1270a4fa
Cedric Cook <cedric.cook@gmail.com>
2014-12-02 09:41:57 +0100

    Refactor code layout

--- ProjectRadinAndroidApp/src/ch/epfl/sweng/radin/RadinGroupBalanceActivity.java
+++ ProjectRadinAndroidApp/src/ch/epfl/sweng/radin/RadinGroupBalanceActivity.java
@@ -41,13 +41,13 @@ import android.widget.Toast;
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_radingroup_balance);
-		
+
 		Bundle extras = getIntent().getExtras();
 		mCurrentRadinGroupModel = ActionBar.getRadinGroupModelFromBundle(extras);
-		
+
 		RelativeLayout thisLayout = (RelativeLayout) findViewById(R.id.balanceRadinGroupLayout);
 		ActionBar.addActionBar(this, thisLayout, mCurrentRadinGroupModel);
-		
+
 		UserStorageManager userStorageManager = UserStorageManager.getStorageManager();
 		userStorageManager.getAllForGroupId(mCurrentRadinGroupModel.getRadinGroupID(),
 				new RadinListener<UserModel>() {
@@ -62,7 +62,7 @@ import android.widget.Toast;
 				}
 			}
 		});
-		
+
 		TransactionWithParticipantsStorageManager storageManager = 
 				TransactionWithParticipantsStorageManager.getStorageManager();
 		storageManager.getAllForGroupId(mCurrentRadinGroupModel.getRadinGroupID(), 
@@ -79,36 +79,36 @@ import android.widget.Toast;
 			}
 		});
 
-		
+
 		drawBalances(calculateBalances());
 	}
-	
+
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu items for use in the action bar
 		MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.radingroup_configuration, menu);
 		return super.onCreateOptionsMenu(menu);
 	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-	    // Handle presses on the action bar items
-	    switch (item.getItemId()) {
-	        case R.id.action_home:
-	        	Intent intent = new Intent(this, HomeActivity.class);
-	        	startActivity(intent);
-	            return true;
-	        case R.id.action_settings:
-	         
-	            return true;
-	        default:
-	            return super.onOptionsItemSelected(item);
-	    }
+		// Handle presses on the action bar items
+		switch (item.getItemId()) {
+		case R.id.action_home:
+			Intent intent = new Intent(this, HomeActivity.class);
+			startActivity(intent);
+			return true;
+		case R.id.action_settings:
+
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
-	
+
 	private HashMap<Integer, Double> calculateBalances() {		
 		HashMap<Integer, Double> userBalances = new HashMap<Integer, Double>();		
-		
+
 		if (mParticipants == null) {
 			try {
 				mParticipants.wait(TIME_OUT);
@@ -117,11 +117,11 @@ import android.widget.Toast;
 				e.printStackTrace();
 			}
 		}
-		
+
 		for (UserModel participant : mParticipants) {
 			userBalances.put(participant.getId(), 0.0);
 		}
-		
+
 		if (mTransactions == null) {
 			try {
 				mTransactions.wait(TIME_OUT);
@@ -130,37 +130,37 @@ import android.widget.Toast;
 				e.printStackTrace();
 			}
 		}
-		
+
 		for (TransactionWithParticipantsModel transaction : mTransactions) {
 			double transactionAmount = transaction.getAmount();
 			Map<Integer, Integer> usersAndCoefficients = transaction.getUsersWithCoefficients();
 			int sumCoefficients = 0;
-			
+
 			for (Integer coefficient : usersAndCoefficients.values()) {
 				sumCoefficients += coefficient;
 			}
-			
+
 			for (Integer participant : userBalances.keySet()) {
 				Double oldBalance = userBalances.get(participant);
 				Double newBalance = transactionAmount * (usersAndCoefficients.get(participant) / sumCoefficients);
-				
+
 				if (transaction.getCreatorID() == participant) {
 					newBalance += transactionAmount;
 				}
-				
+
 				userBalances.put(participant, oldBalance + newBalance);
 			}
 		}
 		return userBalances;
 	}
-	
+
 	private void drawBalances(HashMap<Integer, Double> userBalances) {
 		LinearLayout radinGroupBalanceLinearLayout = (LinearLayout) findViewById(R.id.radinGroupBalanceLinearLayout);
 		radinGroupBalanceLinearLayout.setGravity(Gravity.LEFT);
-		
+
 		ProgressBar progressBar = (ProgressBar) findViewById(R.id.radinGroupBalanceProgressBar);
 		radinGroupBalanceLinearLayout.removeView(progressBar);
-		
+
 		int i = 0;
 		for (UserModel participant : mParticipants) {
 			TextView userBalanceTextView = new TextView(this);
@@ -169,13 +169,13 @@ import android.widget.Toast;
 			userBalanceTextView.setTextSize(TEXT_SIZE);
 			userBalanceTextView.setTag(i);
 			i++;
-			
+
 			radinGroupBalanceLinearLayout.addView(userBalanceTextView);
 		}
 	}
-	
-	
+
+
 	private void displayErrorToast(String message) {
-	    Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+		Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
 	}
 }
